d692c0d7b5868e6c45d93c59e284f35a7955722c
Jim Breslin <jbreslin33@gmail.com>
2018-10-26 11:16:11 -0400

    erm

--- database/erm.sh
+++ database/erm.sh
@@ -1,2 +1,2 @@
-java -jar src/database/schemaSpy.jar -t pgsql -host localhost -db cms -u postgres -p mibesfat -o erm -s public -dp src/database/postgresql.jar
+java -jar database/schemaSpy.jar -t pgsql -host localhost -db cms -u postgres -p mibesfat -o erm -s public -dp database/postgresql.jar
 firefox erm/index.html
