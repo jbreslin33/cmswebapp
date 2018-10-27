dc824bf8a83fb5f5745268e135c529162918700e
Jim Breslin <jbreslin33@gmail.com>
2018-10-26 20:41:50 -0400

    application loaded

--- scripts/application/application.js
+++ scripts/application/application.js
@@ -7,31 +7,15 @@ class Application
     		this.mStateLogs = false;
 
                 //states
-                this.mStateMachine = new StateMachine(this);
+                //this.mStateMachine = new StateMachine(this);
 
-                this.mGLOBAL_APPLICATION  = new GLOBAL_APPLICATION       (this);
-                this.mINIT_APPLICATION    = new INIT_APPLICATION         (this);
-                this.mNORMAL_APPLICATION  = new NORMAL_APPLICATION         (this);
+                //this.mGLOBAL_APPLICATION  = new GLOBAL_APPLICATION       (this);
+                //this.mINIT_APPLICATION    = new INIT_APPLICATION         (this);
+                //this.mNORMAL_APPLICATION  = new NORMAL_APPLICATION         (this);
 	}
-}
-
-  	sayName() 
-	{
-    		ChromeSamples.log('Hi, I am a ', this.name + '.');
-  	}
-
-
-        log(msg)
-        {
-                setTimeout(function()
-                {
-                        throw new Error(msg);
-                }, 0);
-        },
 
         update()
         {
-                this.mStateMachine.update();
-        },
-
+                //this.mStateMachine.update();
+        }
 }
