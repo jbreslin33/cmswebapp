2f649d54de4bc043e0e3cc08cd0e69e70be62150
Jim Breslin <jbreslin33@gmail.com>
2018-10-29 20:11:14 -0400

    order of scripts

--- scripts/application/application.js
+++ scripts/application/application.js
@@ -9,28 +9,18 @@ class Application
     		this.mStateLogs = true;
 
                 //states
-		this.mStateMachine = 0;
-                
 		this.mStateMachine = new StateMachine(this);
 			
                 //this.mGLOBAL_APPLICATION  = new GLOBAL_APPLICATION();
                 this.mINIT_APPLICATION    = new INIT_APPLICATION  ();
                 
-		this.mStateMachine.changeState(this.mINIT_APPLICATION);
-
-                var t=setInterval(this.update,32);
-		
-	}
-
-	//startTimer()
-	//{
                 //this.mStateMachine.setGlobalState(this.mGLOBAL_APPLICATION);
-                //START UPDATING
-	//}
+		this.mStateMachine.changeState(this.mINIT_APPLICATION);
+	}
 
         update()
         {
 		console.log('update called in application');
-                //this.mStateMachine.update();
+                this.mStateMachine.update();
         }
 }
