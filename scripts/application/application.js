3cd737fa3eb566b0df7909e760677b84c6e89564
Jim Breslin <jbreslin33@gmail.com>
2018-10-31 11:11:09 -0400

    remove and add

--- scripts/application/application.js
+++ scripts/application/application.js
@@ -5,8 +5,6 @@ class Application
 
 	constructor() 
 	{
-		console.log("Application constructor");
-
     		var mStateLogs = true;
 
                 //states
@@ -23,39 +21,14 @@ class Application
 
 	update()
 	{
-		console.log('application.update called');
 		this.mStateMachine.update();
 	}
 }
-/*
-class Application 
+
+class Report
 {
-	constructor() 
+	constructor()
 	{
 
 	}
-}
-*/
-class Polygon {
-  // ..and an (optional) custom class constructor. If one is
-  // not supplied, a default constructor is used instead:
-  // constructor() { }
-  constructor(height, width) {
-    this.name = 'Polygon';
-    this.height = height;
-    this.width = width;
-  }
-
-  // Simple class instance methods using short-hand method
-  // declaration
-  sayName() {
-    console.log('Hi, I am a ', this.name + '.');
-  }
-
-  sayHistory() {
-    console.log('"Polygon" is derived from the Greek polus (many) ' +
-      'and gonia (angle).');
-  }
-
-  // We will look at static and subclassed methods shortly
-}
+} 
