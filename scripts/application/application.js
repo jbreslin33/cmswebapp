6ea7e65a7b72529555ec1a62a47e765e4a1a9592
Jim Breslin <jbreslin33@gmail.com>
2018-10-31 09:00:48 -0400

    home

--- scripts/application/application.js
+++ scripts/application/application.js
@@ -1,28 +1,54 @@
-'use strict';
-
 class Application 
 {
+
 	constructor() 
 	{
 		console.log("Application constructor");
 
-    		this.mStateLogs = true;
+    		var mStateLogs = true;
 
                 //states
-		this.mStateMachine = new StateMachine(this);
+		var mStateMachine = new StateMachine(this);
 			
                 //this.mGLOBAL_APPLICATION  = new GLOBAL_APPLICATION();
-                this.mINIT_APPLICATION    = new INIT_APPLICATION  ();
+                var mINIT_APPLICATION    = new INIT_APPLICATION  ();
                 
                 //this.mStateMachine.setGlobalState(this.mGLOBAL_APPLICATION);
-		this.mStateMachine.changeState(this.mINIT_APPLICATION);
+		mStateMachine.changeState(this.mINIT_APPLICATION);
                 
-		this.mStateMachine.update();
+		mStateMachine.update();
 	}
 
-        update()
-        {
-		console.log('update called in application');
-                this.mStateMachine.update();
-        }
+}
+/*
+class Application 
+{
+	constructor() 
+	{
+
+	}
+}
+*/
+class Polygon {
+  // ..and an (optional) custom class constructor. If one is
+  // not supplied, a default constructor is used instead:
+  // constructor() { }
+  constructor(height, width) {
+    this.name = 'Polygon';
+    this.height = height;
+    this.width = width;
+  }
+
+  // Simple class instance methods using short-hand method
+  // declaration
+  sayName() {
+    console.log('Hi, I am a ', this.name + '.');
+  }
+
+  sayHistory() {
+    console.log('"Polygon" is derived from the Greek polus (many) ' +
+      'and gonia (angle).');
+  }
+
+  // We will look at static and subclassed methods shortly
 }
