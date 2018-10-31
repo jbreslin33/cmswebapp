b9fbc3a37265ce84159c90148d012d6c383b8002
Jim Breslin <jbreslin33@gmail.com>
2018-10-31 09:20:04 -0400

    update working

--- scripts/application/application.js
+++ scripts/application/application.js
@@ -8,20 +8,21 @@ class Application
     		var mStateLogs = true;
 
                 //states
-		var mStateMachine = new StateMachine(this);
+		this.mStateMachine = new StateMachine(this);
 			
                 //this.mGLOBAL_APPLICATION  = new GLOBAL_APPLICATION();
                 var mINIT_APPLICATION    = new INIT_APPLICATION  ();
                 
                 //this.mStateMachine.setGlobalState(this.mGLOBAL_APPLICATION);
-		mStateMachine.changeState(this.mINIT_APPLICATION);
+		this.mStateMachine.changeState(this.mINIT_APPLICATION);
                 
-		mStateMachine.update();
+		this.mStateMachine.update();
 	}
 
 	update()
 	{
 		console.log('application.update called');
+		this.mStateMachine.update();
 	}
 }
 /*
