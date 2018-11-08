b2b8e2a6676b4f94681cd9a2ef9cc2cb8979ab8e
Jim Breslin <jbreslin33@gmail.com>
2018-11-08 10:43:51 -0500

    club id not needed

--- database/insert.sql
+++ database/insert.sql
@@ -7,3 +7,7 @@ insert into users (username,password) values ('jbreslin33@gmail.com','mibesfat')
 --teams
 insert into teams (name,club_id) values ('u15boys',1);
 
+--teams_users
+insert into teams_users (user_id,team_id) values (1,1);
+
+
