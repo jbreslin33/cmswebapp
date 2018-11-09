e2aee732a0c2b56b3ec3ae5a82e8ff12d8c1732d
Jim Breslin <jbreslin33@gmail.com>
2018-11-09 12:21:05 -0500

    week

--- php/querys/get_schedule.php
+++ php/querys/get_schedule.php
@@ -4,7 +4,7 @@ session_start();
 include_once(getenv("DOCUMENT_ROOT") . "/php/classes/database/database.php");
 
 $database = new Database();
-$results = $database->query("select event_date, start_time, address from practices WHERE date >= NOW()"); 
+$results = $database->query("select event_date, start_time, address from practices where event_date > now() - INTERVAL '1 days' AND event_date < NOW() + INTERVAL '7 days'");
 $myarray = array();
 while ($row = pg_fetch_row($results)) 
 {
