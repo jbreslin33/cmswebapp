ca1e18c2008d1f2b0a9e39a30c67d99748f60af2
Jim Breslin <jbreslin33@gmail.com>
2018-11-10 19:45:58 -0500

    json

--- php/querys/get_schedule.php
+++ php/querys/get_schedule.php
@@ -4,14 +4,21 @@ session_start();
 include_once(getenv("DOCUMENT_ROOT") . "/php/classes/database/database.php");
 
 $database = new Database();
-$results = $database->query("select event_date, start_time, address from practices where event_date > now() - INTERVAL '1 days' AND event_date < NOW() + INTERVAL '7 days'");
+/*
+ *
+ *
+  select array_to_json(array_agg(row_to_json(t)))
+    from (
+      select id, text from words
+    ) t
+ */
+$results = $database->query("select array_to_json(array_agg(row_to_json(practices))) from ( select event_date, start_time, address from practices where event_date > now() - INTERVAL '1 days' AND event_date < NOW() + INTERVAL '7 days') practices");
 $myarray = array();
 while ($row = pg_fetch_row($results)) 
 {
 	$myarray[] = $row;
-	error_log($row[0]);
+	error_log($row);
 }
-
 echo json_encode($myarray);
 
 
