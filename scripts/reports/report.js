6207af58a418e0488ad5b273ddfd04044d8db499
Jim Breslin <jbreslin33@gmail.com>
2018-11-12 16:19:10 -0500

    updateScheduleCard

--- scripts/reports/report.js
+++ scripts/reports/report.js
@@ -233,6 +233,77 @@ class Report
     		var dataLastUpdated = new Date(data.created);
 		console.log('schedule dataLastUpdated:' + dataLastUpdated);
 
+    		var sunrise = data.channel.astronomy.sunrise;
+    		var sunset = data.channel.astronomy.sunset;
+    		var current = data.channel.item.condition;
+    		var humidity = data.channel.atmosphere.humidity;
+    		var wind = data.channel.wind;
+
+    		var card = this.visibleCards[data.key];
+    		if (!card) 
+		{
+      			card = this.cardTemplate.cloneNode(true);
+      			card.classList.remove('cardTemplate');
+      			card.querySelector('.location').textContent = data.label;
+      			card.removeAttribute('hidden');
+      			this.container.appendChild(card);
+      			this.visibleCards[data.key] = card;
+    		}
+    
+		// Verifies the data provide is newer than what's already visible
+    		// on the card, if it's not bail, if it is, continue and update the
+    		// time saved in the card
+    		var cardLastUpdatedElem = card.querySelector('.card-last-updated');
+    		var cardLastUpdated = cardLastUpdatedElem.textContent;
+    		if (cardLastUpdated) 
+		{
+      			cardLastUpdated = new Date(cardLastUpdated);
+      			// Bail if the card has more recent data then the data
+      			if (dataLastUpdated.getTime() < cardLastUpdated.getTime()) 
+			{
+        			return;
+      			}
+    		}
+    
+		cardLastUpdatedElem.textContent = data.created;
+
+    		card.querySelector('.description').textContent = current.text;
+    		card.querySelector('.date').textContent = current.date;
+    		card.querySelector('.current .icon').classList.add(this.getIconClass(current.code));
+    		card.querySelector('.current .temperature .value').textContent =
+      		Math.round(current.temp);
+    		card.querySelector('.current .sunrise').textContent = sunrise;
+    		card.querySelector('.current .sunset').textContent = sunset;
+    		card.querySelector('.current .humidity').textContent =
+      		Math.round(humidity) + '%';
+    		card.querySelector('.current .wind .value').textContent =
+      		Math.round(wind.speed);
+    		card.querySelector('.current .wind .direction').textContent = wind.direction;
+    		var nextDays = card.querySelectorAll('.future .oneday');
+    		var today = new Date();
+    		today = today.getDay();
+    		for (var i = 0; i < 7; i++) 
+		{
+      			var nextDay = nextDays[i];
+      			var daily = data.channel.item.forecast[i];
+      			if (daily && nextDay) 
+			{
+        			nextDay.querySelector('.date').textContent =
+          			this.daysOfWeek[(i + today) % 7];
+        			nextDay.querySelector('.icon').classList.add(this.getIconClass(daily.code));
+        			nextDay.querySelector('.temp-high .value').textContent =
+          			Math.round(daily.high);
+        			nextDay.querySelector('.temp-low .value').textContent =
+          			Math.round(daily.low);
+      			}
+    		}
+    		if (this.isLoading) 
+		{
+      			this.spinner.setAttribute('hidden', true);
+      			this.container.removeAttribute('hidden');
+      			this.isLoading = false;
+    		}
+
 	}
 
   	updateForecastCard(data) 
@@ -373,7 +444,7 @@ class Report
                                         //results.key = key;
                                         //results.label = label;
                                         //results.created = response.query.created;
-                                        //APPLICATION.mWeekReport.updateForecastCard(results);
+                                        //APPLICATION.mWeekReport.updateScheduleCard(results);
                                 }
                         }
                         else
@@ -381,7 +452,7 @@ class Report
 
                         // Return the initial weather forecast since no data is available.
                         console.log("update schedule from initial");
-                        APPLICATION.mWeekReport.updateForecastCard(APPLICATION.mWeekReport.initialScheduleData);
+                        APPLICATION.mWeekReport.updateScheduleCard(APPLICATION.mWeekReport.initialScheduleData);
                         }
                 };
                 request.open('GET', url);
@@ -638,11 +709,10 @@ class Schedule
         {
                 this.key = '13';
                 this.label = 'SCHEDULE LABEL';
-		/*
+		
                 this.created = '2016-07-22T01:00:00Z';
 
                 this.channel = new Channel();
-		*/
         }
 
 }
