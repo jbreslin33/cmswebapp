baa122df5a7de662a09ebce7f7f5f51d60ce76dd
Jim Breslin <jbreslin33@gmail.com>
2018-11-13 10:32:04 -0500

    query

--- database/query.sql
+++ database/query.sql
@@ -1,13 +1,15 @@
 select row_to_json(t)
-from (
-  select id, name,
-    (
-      select array_to_json(array_agg(row_to_json(d)))
-      from (
-        select event_date, start_time, address  
-        from practices
-        order by event_date asc
-      ) d
-    ) as definitions
-  from clubs
-) t
+	from 
+	(
+    		select id, name,
+    		(
+      			select array_to_json(array_agg(row_to_json(d)))
+      			from 
+			(
+        			select event_date, start_time, address  
+        			from practices
+        			order by event_date asc
+      			) d
+    		) as definitions
+  		from clubs
+	) t
