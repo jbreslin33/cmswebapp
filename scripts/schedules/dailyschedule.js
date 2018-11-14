de52691ac68cb3d58b43558dff3be38cddf2fbb0
Jim Breslin <jbreslin33@gmail.com>
2018-11-14 15:04:37 -0500

    card shows

--- scripts/schedules/dailyschedule.js
+++ scripts/schedules/dailyschedule.js
@@ -1,9 +1,42 @@
 'use strict';
 
-class DailySchedule extends Schedule
+class DailySchedule extends Report
 {
 	constructor(application)
 	{
 		super(application);
+
+                this.mDivDate = document.createElement("DIV");
+                this.mDivDate.setAttribute("class", "date");
+                this.mDivCardTemplate.appendChild(this.mDivDate);
+		this.mDivDate.textContent = "yo";
+
+          	var textScale = document.createTextNode(' mph ');
+               	this.mDivDate.appendChild(textScale);
+
+		console.log('DailySchedule constructor');
+
+
+                //report vars
+                this.isLoading = true;
+                this.visibleCards = {};
+                this.spinner = document.querySelector('.loader');
+                this.cardTemplate = document.querySelector('.cardTemplate');
+                this.container = document.querySelector('.main');
+
+                //var card = this.visibleCards[data.key];
+                //if (!card)
+                //{
+                        var card = this.cardTemplate.cloneNode(true);
+                        card.classList.remove('cardTemplate');
+                        //card.querySelector('.location').textContent = data.label;
+                        //card.querySelector('.location').textContent = "LABEL GOES HERE";
+                        card.removeAttribute('hidden');
+                        this.container.appendChild(card);
+                        this.visibleCards[data.key] = card;
+                //}
+
+
+
 	}
 }
