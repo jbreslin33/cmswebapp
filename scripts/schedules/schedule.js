67d5a9882c797417da560efaa82aada67c20c233
Jim Breslin <jbreslin33@gmail.com>
2018-11-14 11:05:23 -0500

    schedules and reports

---
+++ scripts/schedules/schedule.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class Schedule extends Report
+{
+	constructor(application)
+	{
+		//Application
+		this.mApplication = application;
+	}
+}
---
+++ scripts/schedules/weeklyschedule.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class WeeklySchedule extends Schedule
+{
+	constructor(application)
+	{
+		//Application
+		this.mApplication = application;
+	}
+}
