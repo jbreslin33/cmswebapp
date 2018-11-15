6810e28f99c9920dc9898bada8179ac8f0f21852
Jim Breslin <jbreslin33@gmail.com>
2018-11-15 08:42:34 -0500

    main is now in application

--- scripts/application/application.js
+++ scripts/application/application.js
@@ -6,22 +6,28 @@ class Application
 	constructor() 
 	{
 		this.mName = 'yo';
+
 		/*
     		var mStateLogs = true;
-
                 //states
 		this.mStateMachine = new StateMachine(this);
-			
                 var mINIT_APPLICATION    = new INIT_APPLICATION  ();
-                
 		this.mStateMachine.changeState(this.mINIT_APPLICATION);
-                
 		this.mStateMachine.update();
 		*/
-		//this.mWeekReport = new Report(this);
+
+		
 		this.mHeader = new Header(this,"Club Management System");
+
+		/* this is the div container for all the cards. The cards hold the individual reports
+		*/
+                //main class
+                this.mDivMain = document.createElement("DIV");
+                this.mDivMain.setAttribute("class", "main");
+                document.body.appendChild(this.mDivMain);
+		
 		this.mDailySchedule = new DailySchedule(this);
-		//this.mWeekReport = new OldReport(this);
+
 	}
 
 	update()
