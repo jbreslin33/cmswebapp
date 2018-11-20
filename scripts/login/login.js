249b67b38bc08998dc2d5d2da07c8ee7b5b3bb2f
Jim Breslin <jbreslin33@gmail.com>
2018-11-20 14:04:20 -0500

    password

--- scripts/login/login.js
+++ scripts/login/login.js
@@ -12,9 +12,29 @@ class Login extends Report
                 this.mContainer.appendChild(this.mDivCard);
 
 		//login stuff
+		//FORM
+                this.mDivForm = document.createElement("FORM");
+                this.mDivForm.setAttribute("action", "/php/classes/login/login.php");
+                this.mDivForm.setAttribute("method", "post");
+		this.mDivCard.appendChild(this.mDivForm);
+
+                this.mDivLogin = document.createElement("DIV");
+                this.mDivLogin.setAttribute("class", "login");
+		this.mDivForm.appendChild(this.mDivLogin);
+                
+		this.mDivInputEmail = document.createElement("DIV");
+                this.mDivInputEmail.setAttribute("type", "password");
+                this.mDivInputEmail.setAttribute("placeholder", "password");
+                this.mDivInputEmail.setAttribute("id", "password");
+		this.mDivLogin.appendChild(this.mDivInputEmail);
+
+
+		//DUMMY DIV
                 this.mDivBreslin = document.createElement("DIV");
                 this.mDivBreslin.setAttribute("class", "breslin");
 		this.mDivCard.appendChild(this.mDivBreslin);
+
+
 	
 		
 	}
