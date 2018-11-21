adb29c880d144c72846f3591af062a105f8c694c
Jim Breslin <jbreslin33@gmail.com>
2018-11-21 14:05:47 -0500

    checks

--- scripts/login/login.js
+++ scripts/login/login.js
@@ -5,7 +5,6 @@ class Login extends Report
 	constructor(application)
 	{
 		super(application);	
-		console.log('Login Con');
               
 		this.mLoggedIn = false;
 
@@ -35,14 +34,14 @@ class Login extends Report
 		this.mButton = document.createElement("BUTTON");
 		this.mButton.addEventListener("click",this.sendLogin);
 		this.mDivLogin.appendChild(this.mButton);
-/*	
+
+		/*	
 		this.mDivA = document.createElement("A");
                 this.mDivA.setAttribute("href", "#");
                 this.mDivA.setAttribute("class", "forgot");
 		this.mDivA.innerHTML = "forgot password";
 		this.mDivLogin.appendChild(this.mDivA);
 		*/
-		
 	}
 
 	processLogin(code)
@@ -59,51 +58,24 @@ class Login extends Report
 	{
 		console.log('username:' + APPLICATION.mLogin.mDivInputEmail.value); 
 		var url = "/php/classes/login/login.php?username=" + APPLICATION.mLogin.mDivInputEmail.value + "&password=" + APPLICATION.mLogin.mDivInputPassword.value; 
-		console.log('url:' + url);
 
                 // Fetch the latest data.
                 var request = new XMLHttpRequest();
                 request.onreadystatechange = function()
                 {
-			console.log('onready');
                         if (request.readyState === XMLHttpRequest.DONE)
                         {
-				console.log('DONE');
                                 if (request.status === 200)
                                 {
-					console.log('200');
                                         var data = this.responseText;
                                         if (data)
                                         {
 						APPLICATION.mLogin.processLogin(data);
-						console.log('data:' + data);
-                                        }
-                                        else
-                                        {
-                                                console.log('got response with no data');
                                         }
                                 }
                         }
-                        else
-                        {
-                                // Return the initial weather forecast since no data is available.
-                                if (APPLICATION)
-                                {
-                                        console.log('no data yet but APPLICATION exists');
-                                }
-				else
-				{
-                                        console.log('no data yet and APPLICATION does not exist');
-                        	}
-			}
-					
                 };
-
-//		              xmlhttp.open("POST","../../src/php/application/core_application.php?code=117&username=" + username + "&password=" + password,true);
-
-                //request.open('GET', url);
                 request.open('POST', url);
                 request.send();
-
 	}
 }
