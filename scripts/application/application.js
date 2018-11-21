26d95a6c72b05b3220c2cf6c68fde1bc2ebedc56
Jim Breslin <jbreslin33@gmail.com>
2018-11-21 14:14:38 -0500

    states

--- scripts/application/application.js
+++ scripts/application/application.js
@@ -5,6 +5,8 @@ class Application
 	constructor() 
 	{
 
+		this.mStateLogs = true;
+
 		/* this is the div container for all the cards. The cards hold the individual reports
 		*/
 		//header	
@@ -14,6 +16,15 @@ class Application
                 this.mDivMain = document.createElement("DIV");
                 this.mDivMain.setAttribute("class", "main");
                 document.body.appendChild(this.mDivMain);
+
+
+		//states
+		this.mStateMachine = new StateMachine(this);
+		this.mGLOBAL_APPLICATION = new GLOBAL_APPLICATION();
+		this.mINIT_APPLICATION = new INIT_APPLICATION();
+
+		this.mStateMachine.setGlobalState(this.mGLOBAL_APPLICATION);
+		this.mStateMachine.changeState(this.mINIT_APPLICATION);
 
 
 		//login first
