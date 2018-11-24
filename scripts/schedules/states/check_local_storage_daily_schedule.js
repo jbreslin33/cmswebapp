714391aee7d3de4b8688f4937132acd6d0c29f7c
Jim Breslin <jbreslin33@gmail.com>
2018-11-24 17:54:28 -0500

    getting localstorage on load

--- scripts/schedules/states/check_local_storage_daily_schedule.js
+++ scripts/schedules/states/check_local_storage_daily_schedule.js
@@ -14,12 +14,13 @@ class CHECK_LOCAL_STORAGE_DAILY_SCHEDULE extends State
 		{
 			console.log("CHECK_LOCAL_STORAGE_DAILY_SCHEDULE: ENTER");        
 		}
+
+		//basically we are either using initial or localstorage then regardless we are going to internet state
+
                 daily_schedule.mPractice = localStorage.mPractice;
 		if (daily_schedule.mPractice)
 		{
-			console.log('got local mPractice');
-			//we have some local stored data go to that state
-			console.log('mPractice:' + daily_schedule.mPractice);
+			//local storage
 			var data = JSON.parse(daily_schedule.mPractice);
 
 			daily_schedule.mDivEventDate.textContent = data.mEventDate;
@@ -28,14 +29,15 @@ class CHECK_LOCAL_STORAGE_DAILY_SCHEDULE extends State
 		}
 		else
 		{
-			//we have nothing. post up default
+			//initial
 			daily_schedule.mDivEventDate.textContent = daily_schedule.mInitialPractice.mEventDate;
 			daily_schedule.mDivStartTime.textContent = daily_schedule.mInitialPractice.mStartTime;
 			daily_schedule.mDivAddress.textContent = daily_schedule.mInitialPractice.mAddress;
 			daily_schedule.saveToLocalStorage(daily_schedule.mIntialPractice);
-			//go to getInternetData state
-			daily_schedule.mStateMachine.changeState(daily_schedule.mGET_INTERNET_DATA_DAILY_SCHEDULE);
 		}
+
+		//go to getInternetData state
+		daily_schedule.mStateMachine.changeState(daily_schedule.mGET_INTERNET_DATA_DAILY_SCHEDULE);
 	}
 
         execute(daily_schedule)
