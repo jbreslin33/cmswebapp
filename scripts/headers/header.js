a7d5d434f4786ff974cd6ed28172e2e0a15b7ca9
Jim Breslin <jbreslin33@gmail.com>
2018-11-26 11:27:54 -0500

    headers

--- scripts/headers/header.js
+++ scripts/headers/header.js
@@ -36,41 +36,6 @@ class Header
 		this.mButtonAddA.setAttribute("aria-label", "Add");
 		this.mButtonAddA.setAttribute("id", "butAdd");
 		this.mHeader.appendChild(this.mButtonAddA);
-		
-		//addbutton 1
-		this.mButtonAddB = document.createElement("BUTTON");
-		this.mButtonAddB.setAttribute("class", "headerButton");
-		this.mButtonAddB.setAttribute("aria-label", "Add");
-		this.mButtonAddB.setAttribute("id", "butAdd");
-		this.mHeader.appendChild(this.mButtonAddB);
-		
-		//addbutton 1
-		this.mButtonAddC = document.createElement("BUTTON");
-		this.mButtonAddC.setAttribute("class", "headerButton");
-		this.mButtonAddC.setAttribute("aria-label", "Add");
-		this.mButtonAddC.setAttribute("id", "butAdd");
-		this.mHeader.appendChild(this.mButtonAddC);
-		
-		//addbutton 1
-		this.mButtonAddD = document.createElement("BUTTON");
-		this.mButtonAddD.setAttribute("class", "headerButton");
-		this.mButtonAddD.setAttribute("aria-label", "Add");
-		this.mButtonAddD.setAttribute("id", "butAdd");
-		this.mHeader.appendChild(this.mButtonAddD);
-		
-		//addbutton 1
-		this.mButtonAddE = document.createElement("BUTTON");
-		this.mButtonAddE.setAttribute("class", "headerButton");
-		this.mButtonAddE.setAttribute("aria-label", "Add");
-		this.mButtonAddE.setAttribute("id", "butAdd");
-		this.mHeader.appendChild(this.mButtonAddE);
-		
-		//addbutton 1
-		this.mButtonAddF = document.createElement("BUTTON");
-		this.mButtonAddF.setAttribute("class", "headerButton");
-		this.mButtonAddF.setAttribute("aria-label", "Add");
-		this.mButtonAddF.setAttribute("id", "butAdd");
-		this.mHeader.appendChild(this.mButtonAddF);
 	}
 
 	buttonRefreshClicked()
