b249d266fa05bfceee9dc2d5c631a15993f937a0
Jim Breslin <jbreslin33@gmail.com>
2018-11-27 17:40:46 -0500

    time

--- scripts/schedules/states/get_internet_data_schedule.js
+++ scripts/schedules/states/get_internet_data_schedule.js
@@ -43,6 +43,8 @@ class GET_INTERNET_DATA_SCHEDULE extends State
 							practice.mPracticeScreen = new PracticeScreen(practice);
 
 							//set practice member vars
+							var time = new Time();
+							time.convertFromMilitaryToHuman(data[i][1]);
                                                 	practice.mEventDate = data[i][0];
                                                 	practice.mStartTime = data[i][1];
                                                 	practice.mAddress   = data[i][2];
---
+++ scripts/utilities/time.js
@@ -0,0 +1,43 @@
+'use strict';
+
+class Time
+{
+        constructor()
+        {
+
+        }
+	convertFromMilitaryToHuman(time)
+	{
+		time = time.split(':'); // convert to array
+
+		// fetch
+		var hours = Number(time[0]);
+		var minutes = Number(time[1]);
+		var seconds = Number(time[2]);
+
+		// calculate
+		var timeValue;
+
+		if (hours > 0 && hours <= 12) 
+		{
+  			timeValue= "" + hours;
+		} 
+		else if (hours > 12) 
+		{
+  			timeValue= "" + (hours - 12);
+		} 
+		else if (hours == 0) 
+		{
+  			timeValue= "12";
+		}
+ 
+		timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
+		timeValue += (seconds < 10) ? ":0" + seconds : ":" + seconds;  // get seconds
+		timeValue += (hours >= 12) ? " P.M." : " A.M.";  // get AM/PM
+
+		// show
+		//alert(timeValue);
+		console.log(timeValue);
+		return timeValue;
+	}
+}
