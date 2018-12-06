4c379aaf99243d1849d5ce0774bb5aa9eafbc1ab
Jim Breslin <jbreslin33@gmail.com>
2018-12-06 09:53:09 -0500

    hard coded username

--- php/querys/get_schedule.php
+++ php/querys/get_schedule.php
@@ -4,7 +4,7 @@ session_start();
 include_once(getenv("DOCUMENT_ROOT") . "/php/classes/database/database.php");
 
 $query = "
-select event_date, start_time, address from practices order by event_date asc 
+select event_date, start_time, practices.address from practices join teams on teams.id=practices.team_id join teams_users on teams_users.team_id=teams.id join users on users.id=teams_users.user_id where users.username = 'j' order by event_date asc
 ";
 $database = new Database();
 
