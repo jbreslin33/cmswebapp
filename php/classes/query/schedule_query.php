8edf8465595448dba0d9a6a6e4aa0cbabaaeea58
Jim Breslin <jbreslin33@gmail.com>
2018-12-21 14:00:31 -0500

    teams

--- php/classes/query/schedule_query.php
+++ php/classes/query/schedule_query.php
@@ -11,7 +11,7 @@ class ScheduleQuery extends Query
 	public function query()
 	{
 		$this->mQuery = "
-		select affair_date, arrival_time, start_time, end_time, affairs.address, affairs.coordinates, pitches.name, field_name, affairs.team_id, affair_types.name from affairs FULL OUTER JOIN teams on teams.id=affairs.team_id FULL OUTER JOIN teams_users on teams_users.team_id=teams.id FULL OUTER JOIN users on users.id=teams_users.user_id FULL OUTER JOIN affair_types on affair_types.id=affairs.affair_types_id FULL OUTER JOIN pitches on pitches.id=affairs.pitch_id where users.username = '" .
+		select affair_date, arrival_time, start_time, end_time, affairs.address, affairs.coordinates, pitches.name, field_name, teams.name, affair_types.name from affairs FULL OUTER JOIN teams on teams.id=affairs.team_id FULL OUTER JOIN teams_users on teams_users.team_id=teams.id FULL OUTER JOIN users on users.id=teams_users.user_id FULL OUTER JOIN affair_types on affair_types.id=affairs.affair_types_id FULL OUTER JOIN pitches on pitches.id=affairs.pitch_id where users.username = '" .
 		$this->mUsername .
 		"' order by affair_date asc";
 		error_log($this->mQuery);
