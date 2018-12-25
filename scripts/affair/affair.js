70ab036b78aff376e9c95e499305be6cc3582d84
Jim Breslin <jbreslin33@gmail.com>
2018-12-25 13:25:36 -0500

    delete

--- scripts/affair/affair.js
+++ scripts/affair/affair.js
@@ -6,16 +6,16 @@ class Affair
 	{
 		this.mSchedule = schedule;
 
-                this.mAffairDate = 'JAN 01 2000';
-                this.mArrivalTime = '11PM';
-                this.mStartTime = '12AM';
-                this.mEndTime = null;
-                this.mAddress   = 'NOWHERE';
-                this.mCoordinates   = null;
-                this.mPitch   = null;
-                this.mFieldName   = null;
-                this.mTeam   = null;
-                this.mAffairType = 'Practice';
+                this.mAffairDate   = 'JAN 01 2000';
+                this.mArrivalTime  = '11PM';
+                this.mStartTime    = '12AM';
+                this.mEndTime      = null;
+                this.mAddress      = 'NOWHERE';
+                this.mCoordinates  = null;
+                this.mPitch        = null;
+                this.mFieldName    = null;
+                this.mTeam         = null;
+                this.mAffairType   = 'Practice';
 		
 		this.mAffairScreen = null;
 	}
