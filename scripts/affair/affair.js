a2caf2d959d2efb02dab45ff75920be651bddce5
Jim Breslin <jbreslin33@gmail.com>
2018-12-28 14:09:33 -0500

    using arrays

--- scripts/affair/affair.js
+++ scripts/affair/affair.js
@@ -7,18 +7,6 @@ class Affair
 		this.mSchedule = schedule;
 
 		this.mData = new Array();
-/*
-                this.mAffairDate   = 'JAN 01 2000';
-                this.mArrivalTime  = '11PM';
-                this.mStartTime    = '12AM';
-                this.mEndTime      = null;
-                this.mAddress      = 'NOWHERE';
-                this.mCoordinates  = null;
-                this.mPitch        = null;
-                this.mFieldName    = null;
-                this.mTeam         = null;
-                this.mAffairType   = 'Practice';
-*/		
 		this.mScreen = null;
 	}
 	destructor()
