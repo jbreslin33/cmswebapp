d73edcaefcf3c8e9d2a71374249ca2b67ce7f6e1
Jim Breslin <jbreslin33@gmail.com>
2018-12-29 14:28:42 -0500

    sql

--- database/query.sql
+++ database/query.sql
@@ -1,5 +1,7 @@
 select users.id, clubs_users.user_id, pitches.name
 from 
 users
-FULL OUTER JOIN clubs_users on clubs_users.user_id=users.id full outer join clubs on clubs.id=clubs_users.club_id full outer join pitches on pitches.club_id=clubs.id
+full outer join clubs_users on clubs_users.user_id=users.id 
+full outer join clubs on clubs.id=clubs_users.club_id 
+full outer join pitches on pitches.club_id=clubs.id
 where users.username = 'l';
