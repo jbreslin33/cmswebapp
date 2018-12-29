abb7a021f2e4d0ccc573d3afac992ceb1d6d4209
Jim Breslin <jbreslin33@gmail.com>
2018-12-29 17:08:59 -0500

    affair type

--- php/classes/query/affair_type_query.php
+++ php/classes/query/affair_type_query.php
@@ -11,14 +11,7 @@ class AffairTypeQuery extends Query
 	public function query()
 	{
 		$this->mQuery = "
-			select pitches.id, pitches.name
-			from users
-			full outer join clubs_users on clubs_users.user_id=users.id
-			full outer join clubs on clubs.id=clubs_users.club_id
-			full outer join pitches on pitches.club_id=clubs.id
-			where users.username = '" .
-			$this->mUsername .
-			"' order by pitches.name asc";
+			select id, name from affair_types order by name asc";
 			error_log($this->mQuery);
 	}
 }
