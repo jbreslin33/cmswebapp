cfbd10fe8c7ab88e568ce36083e3d5b2871faa00
Jim Breslin <jbreslin33@gmail.com>
2018-12-29 15:25:03 -0500

    query

--- database/query.sql
+++ database/query.sql
@@ -3,4 +3,4 @@ from
 users
 full outer join teams_users on teams_users.user_id=users.id 
 full outer join teams on teams.id=teams_users.team_id 
-where users.username = 'l';
+where users.username = 'lo';
