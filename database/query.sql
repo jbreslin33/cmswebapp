83e15ce328b775240118e115ea4b8cbd94107294
Jim Breslin <jbreslin33@gmail.com>
2019-01-02 13:06:16 -0500

    query to get fields using club id and users roles club tables and default timestamp

--- database/query.sql
+++ database/query.sql
@@ -64,3 +64,23 @@ where users.username = 'lo'
 and clubs.id = 1 
 order by pitches.name asc;
 
+
+select pitches.id, pitches.name
+from users
+full outer join clubs_users on clubs_users.user_id=users.id
+full outer join clubs on clubs.id=clubs_users.club_id
+full outer join pitches on pitches.club_id=clubs.id
+where users.username = 'lo'
+and clubs.id = 
+	(
+	SELECT club_id 
+	from users_clubs_roles 
+	order by users_clubs_roles.default_timestamp desc limit 1 
+	)
+order by pitches.name asc;
+
+
+
+
+
+
