e4542413e4bf7a1894af535066994359d2a7fd4e
Jim Breslin <jbreslin33@gmail.com>
2019-01-02 11:22:28 -0500

    query

--- database/query.sql
+++ database/query.sql
@@ -55,4 +55,12 @@ full outer join teams on teams.id=users_clubs_roles_teams.team_id
 where users.username = 'l'
 order by users_clubs_roles_teams.team_id asc;
 
+select pitches.id, pitches.name
+from users
+full outer join clubs_users on clubs_users.user_id=users.id
+full outer join clubs on clubs.id=clubs_users.club_id
+full outer join pitches on pitches.club_id=clubs.id
+where users.username = 'lo'
+and clubs.id = 1 
+order by pitches.name asc;
 
