134bf28153803af662146766df3cb1d9df4e71c2
Jim Breslin <jbreslin33@gmail.com>
2019-01-10 17:57:43 -0500

    back

--- scripts/schedules/states/get_internet_data_schedule.js
+++ scripts/schedules/states/get_internet_data_schedule.js
@@ -14,7 +14,62 @@ class GET_INTERNET_DATA_SCHEDULE extends State
 		{
 			console.log("GET_INTERNET_DATA_SCHEDULE: ENTER");        
 		}
-		schedule.getSchedule();
+                var url = "/php/classes/query/schedule_query.php?username=" + APPLICATION.mLogin.mUsername;
+
+                // Fetch the latest data.
+                schedule.mRequest= new XMLHttpRequest();
+                schedule.mRequest.onreadystatechange = function()
+                {
+                        if (schedule.mRequest.readyState === XMLHttpRequest.DONE)
+                        {
+                                if (schedule.mRequest.status === 200)
+                                {
+					var code = this.responseText.slice(0,4);
+					var data = this.responseText.slice(4,this.responseText.length);
+                                        var jsondata = JSON.parse(data);
+					
+                                        if (jsondata)
+                                        {
+						//lets clear array....
+						var i = 0;
+						for (i = 0; i < APPLICATION.mSchedule.mSelectAffairArray.length; i++)
+						{
+							var affair = APPLICATION.mSchedule.mSelectAffairArray.shift();
+							affair.mScreen.mDivCard.style.display = "none";
+						}
+						
+						i = 0;
+						while (jsondata[i])
+						{
+							var affair = new SelectAffair(schedule);
+							for (var b = 0; b < 10; b++)
+							{
+								affair.mData.push(jsondata[i][b]);
+							}
+
+							//create screen to display data
+							affair.mScreen = new SelectAffairScreen(affair);
+
+							//update screen card
+                                                	affair.mScreen.update();
+				
+							//save for later
+							APPLICATION.mSchedule.saveToLocalStorage(affair);
+
+							//push to array
+							APPLICATION.mSchedule.mSelectAffairArray.push(affair);
+							i++;
+						}
+                                        }
+                                        else
+                                        {
+                                                console.log('no schedule');
+                                        }
+                                }
+                        }
+                };
+                schedule.mRequest.open('GET', url);
+                schedule.mRequest.send();
 	}
 
         execute(schedule)
