7c9f6176c9a47b86be20af6e28f41c3ab104787e
Jim Breslin <jbreslin33@gmail.com>
2019-01-11 14:12:48 -0500

    closeer

--- php/classes/update/availability_update.php
+++ php/classes/update/availability_update.php
@@ -5,17 +5,20 @@ class AvailabilityUpdate extends Update
 {
 	function __construct() 
 	{
-		parent::__construct();
 		
 		if (isset($_GET['affairs_users_availability_id']))
                 {
                         $this->mAffairsUsersAvailabilityID = $_GET['affairs_users_availability_id'];
+			error_log('got 1');
                 }
 
 		if (isset($_GET['availability_id']))
                 {
                         $this->mAvailabilityID = $_GET['availability_id'];
+			error_log('got 2');
                 }
+
+		parent::__construct();
 	}
 
 	public function update()
