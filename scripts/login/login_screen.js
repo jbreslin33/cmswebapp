623863b92705dda085a1834cc6f2506eeedb9920
Jim Breslin <jbreslin33@gmail.com>
2019-01-15 19:59:31 -0500

    login

--- scripts/login/login_screen.js
+++ scripts/login/login_screen.js
@@ -12,18 +12,15 @@ class LoginScreen
                 this.mContainer = document.createElement("DIV");
                 this.mContainer.setAttribute("class", "container");
                
-		//input-group
+		//email
 		this.mEmailInputGroup = document.createElement("DIV");
                 this.mEmailInputGroup.setAttribute("class", "input-group");
 
-
-		//email
 		this.mEmailInputGroupAddon = document.createElement("SPAN");
 		this.mEmailInputGroupAddon.setAttribute("class","input-group-addon");
-
+	
 		this.mGlyphiconUser = document.createElement("I");
 		this.mGlyphiconUser.setAttribute("class","glyphicon glyphicon-user");
-
 		
 		this.mEmail = document.createElement("INPUT");
 		this.mEmail.setAttribute("type","text");
@@ -31,12 +28,32 @@ class LoginScreen
 		this.mEmail.setAttribute("name","email");
 		this.mEmail.setAttribute("placeholder","Email");
 
+		//password
+		this.mPasswordInputGroup = document.createElement("DIV");
+                this.mPasswordInputGroup.setAttribute("class", "input-group");
+
+		this.mPasswordInputGroupAddon = document.createElement("SPAN");
+		this.mPasswordInputGroupAddon.setAttribute("class","input-group-addon");
+	
+		this.mGlyphiconLock = document.createElement("I");
+		this.mGlyphiconLock.setAttribute("class","glyphicon glyphicon-lock");
+
+		this.mPassword = document.createElement("INPUT");
+		this.mPassword.setAttribute("type","password");
+		this.mPassword.setAttribute("class","form-control");
+		this.mPassword.setAttribute("name","password");
+		this.mPassword.setAttribute("placeholder","Password");
+
 		//attach elements
 		document.body.appendChild(this.mContainer);
 			this.mContainer.appendChild(this.mEmailInputGroup);
 				this.mEmailInputGroup.appendChild(this.mEmailInputGroupAddon);
 					this.mEmailInputGroupAddon.appendChild(this.mGlyphiconUser);
 				this.mEmailInputGroup.appendChild(this.mEmail);
+			this.mContainer.appendChild(this.mPasswordInputGroup);
+				this.mPasswordInputGroup.appendChild(this.mPasswordInputGroupAddon);
+					this.mPasswordInputGroupAddon.appendChild(this.mGlyphiconLock);
+				this.mPasswordInputGroup.appendChild(this.mPassword);
 	}
 
 	sendLogin()
