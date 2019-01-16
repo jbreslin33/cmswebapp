5446b4b835f7b5f51128d49d57c19cbb8bf492ce
Jim Breslin <jbreslin33@gmail.com>
2019-01-15 19:54:30 -0500

    login

--- scripts/login/login_screen.js
+++ scripts/login/login_screen.js
@@ -8,10 +8,23 @@ class LoginScreen
 		this.mApplication = application;
 
 		//login stuff
+		//container
                 this.mContainer = document.createElement("DIV");
                 this.mContainer.setAttribute("class", "container");
+               
+		//input-group
+		this.mEmailInputGroup = document.createElement("DIV");
+                this.mEmailInputGroup.setAttribute("class", "input-group");
+
 
 		//email
+		this.mEmailInputGroupAddon = document.createElement("SPAN");
+		this.mEmailInputGroupAddon.setAttribute("class","input-group-addon");
+
+		this.mGlyphiconUser = document.createElement("I");
+		this.mGlyphiconUser.setAttribute("class","glyphicon glyphicon-user");
+
+		
 		this.mEmail = document.createElement("INPUT");
 		this.mEmail.setAttribute("type","text");
 		this.mEmail.setAttribute("class","form-control");
@@ -20,7 +33,10 @@ class LoginScreen
 
 		//attach elements
 		document.body.appendChild(this.mContainer);
-		this.mContainer.appendChild(this.mEmail);
+			this.mContainer.appendChild(this.mEmailInputGroup);
+				this.mEmailInputGroup.appendChild(this.mEmailInputGroupAddon);
+					this.mEmailInputGroupAddon.appendChild(this.mGlyphiconUser);
+				this.mEmailInputGroup.appendChild(this.mEmail);
 	}
 
 	sendLogin()
