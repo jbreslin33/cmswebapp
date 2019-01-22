881cd2f7553744a8e46561764d6dcc819f254f13
Jim Breslin <jbreslin33@gmail.com>
2019-01-22 14:33:59 -0500

    rm

--- setup_angular.sh
+++
@@ -1,2 +0,0 @@
-sudo npm install -g @angular/cli
-sudo npm install -g npm
--- setup_bootstrap.sh
+++
@@ -1,5 +0,0 @@
-echo install popper.js jquery.js and bootstrap
-echo sudo npm install popper.js
-echo sudo npm install jquery.js
-echo sudo npm install bootstrap
-
