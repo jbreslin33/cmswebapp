c21332a889312f151c3c86a5b1a38d884bb3efd4
Jim Breslin <jbreslin33@gmail.com>
2019-01-25 16:00:53 -0500

    home

--- php/classes/insert/insert_evento.php
+++ php/classes/insert/insert_evento.php
@@ -10,11 +10,14 @@ class InsertEvento extends Insert
 
 	public function query()
 	{
+		/*
 		$this->mSQL = "
 		select evento_date, arrival_time, start_time, end_time, eventos.address, eventos.coordinates, pitches.name, field_name, teams.name, evento_types.name from eventos FULL OUTER JOIN teams on teams.id=eventos.team_id FULL OUTER JOIN teams_users on teams_users.team_id=teams.id FULL OUTER JOIN users on users.id=teams_users.user_id FULL OUTER JOIN evento_types on evento_types.id=eventos.evento_types_id FULL OUTER JOIN pitches on pitches.id=eventos.pitch_id where users.username = '" .
 		$this->mUsername .
 		"' order by evento_date asc";
-
+		 */
+		$this->mSQL = "
+		insert into eventos (address) 
 		error_log($this->mSQL);
 	}
 }
