8df18917d44927792a206cc29146fbdc85909ce4
Jim Breslin <jbreslin33@gmail.com>
2019-02-05 15:43:53 -0500

    query is on way

--- database/query.sql
+++ database/query.sql
@@ -6,11 +6,12 @@
                         from eventos
 
 			full outer join pitches on pitches.id=eventos.pitch_id
-			full outer join teams on teams.id=eventos.team_id
 			full outer join evento_types on evento_types.id=eventos.evento_types_id
 
                         full outer join eventos_players_availability on eventos_players_availability.evento_id=eventos.id
                         full outer join availability on availability.id=eventos_players_availability.availability_id
+			
+			full outer join teams on teams.id=eventos.team_id
 
                         where evento_date >= now()
                         --and users.username = 'l'
