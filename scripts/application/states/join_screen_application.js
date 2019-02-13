d527cc2aeabc45ad5016aad0b8900c6cffa4e36a
Jim Breslin <jbreslin33@gmail.com>
2019-02-13 12:43:01 -0500

    join

---
+++ scripts/application/states/join_screen_application.js
@@ -0,0 +1,49 @@
+
+'use strict';
+
+class SHOW_DAILY_SCHEDULE_APPLICATION extends State
+{
+	constructor() 
+	{
+		super();
+	}
+
+        enter(app)
+        {
+		if (app.mStateLogs || app.mStateEnterLogs)
+		{
+			console.log("SHOW_DAILY_SCHEDULE_APPLICATION: ENTER");        
+		}
+		if (app.mDailySchedule)
+		{
+			//also maybe some clean up as well, so just leaving this if statement here.
+			app.mDailySchedule = new DailySchedule(app);
+
+		}
+		else
+		{
+			app.mDailySchedule = new DailySchedule(app);
+		}
+		document.getElementById("daily_schedule_nav_id").className += " active";
+	}
+
+        execute(app)
+        {
+		if (app.mStateLogs || app.mStateExecuteLogs)
+		{
+			console.log("SHOW_DAILY_SCHEDULE_APPLICATION: EXECUTE");        
+		}
+	}
+
+        exit(app)
+        {
+		if (app.mStateLogs || app.mStateExitLogs)
+		{
+			console.log("SHOW_DAILY_SCHEDULE_APPLICATION: EXIT");        
+		}
+		//hide it for now maybe delete later
+		app.mDailySchedule.hideSchedule();
+
+		document.getElementById("daily_schedule_nav_id").className = document.getElementById("daily_schedule_nav_id").className.replace(" active", "");
+	}
+}
