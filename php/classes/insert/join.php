c66be783f5db5bea5f7bc015774e58f63a20afcc
Jim Breslin <jbreslin33@gmail.com>
2019-02-25 09:44:12 -0500

    calling stored procedure from php

--- php/classes/insert/join.php
+++ php/classes/insert/join.php
@@ -1,10 +1,5 @@
 <?php 
-
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/tuples/email_tuple.php");
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/tuples/login_tuple.php");
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/tuples/person_tuple.php");
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/tuples/person_login_tuple.php");
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/tuples/person_email_tuple.php");
+include_once(getenv("DOCUMENT_ROOT") . "/php/classes/database/database.php");
 
 class Join 
 {
@@ -54,97 +49,24 @@ class Join
                         $this->mPassword = $_GET['password'];
                 }
 
-		$this->mError = false;
-	
-		
-
-/*
-		$this->mEmailTuple = new EmailTuple();
-		$this->mEmailTuple->insert($this->mEmail);
-
-		//insert next tuple
-		if ($this->mEmailTuple->mInsert->getSuccess())
-		{
-			$this->mLoginTuple = new LoginTuple();
-
-			$this->mLoginTuple->insert($this->mEmailTuple->mID, $this->mPassword);
-		}
-		else
-		{
-			$this->mError = true;
-		}
-
-		if ($this->mError == false)
-		{
-			if ($this->mLoginTuple->mInsert->getSuccess())
-			{
-				$this->mPersonTuple = new PersonTuple();
-				$this->mPersonTuple->insert($this->mFirstName, $this->mMiddleName, $this->mLastName, $this->mPhone, $this->mAddress);
-			}
-			else
-			{
-				$this->mError = true;
-				$this->mEmailTuple->delete($this->mEmailTuple->mID);
-			}
-		}
-		
-		if ($this->mError == false)
-                {
-                        if ($this->mPersonTuple->mInsert->getSuccess())
-                        {
-                                $this->mPersonLoginTuple = new PersonLoginTuple();
-                                $this->mPersonLoginTuple->insert($this->mPersonTuple->mID, $this->mLoginTuple->mID);
-                        }
-                        else
-                        {
-				$this->mError = true;
-				$this->mEmailTuple->delete($this->mEmailTuple->mID);
-                                $this->mLoginTuple->delete($this->mLoginTuple->mID);
-                        }
-                }
-		
-		if ($this->mError == false)
-                {
-                        if ($this->mPersonLoginTuple->mInsert->getSuccess())
-                        {
-                                $this->mPersonEmailTuple = new PersonEmailTuple();
-                                $this->mPersonEmailTuple->insert($this->mPersonTuple->mID, $this->mEmailTuple->mID);
-                        }
-                        else
-                        {
-				$this->mError = true;
-				$this->mEmailTuple->delete($this->mEmailTuple->mID);
-                                $this->mLoginTuple->delete($this->mLoginTuple->mID);
-                                $this->mPersonTuple->delete($this->mLoginTuple->mID);
-                        }
-                }
-		
-		if ($this->mError == false)
-                {
-                        if ($this->mPersonEmailTuple->mInsert->getSuccess())
-                        {
-				//do nothing we had success
-                        }
-                        else
-                        {
-				$this->mError = true;
-				$this->mEmailTuple->delete($this->mEmailTuple->mID);
-                                $this->mLoginTuple->delete($this->mLoginTuple->mID);
-                                $this->mPersonTuple->delete($this->mLoginTuple->mID);
-                                $this->mPersonLoginTuple->delete($this->mPersonLoginTuple->mID);
-                        }
-                }
-		
-		if ($this->mError == false)
-		{
-			echo "100";	
-		}
-		else
-		{
-			echo "101";	
-		}
- */
+		//$this->mSQL = "joinsite($this->mEmail
+		$this->run();
 	}
+
+        public function run()
+        {
+                $database = new Database("localhost","cms","postgres","mibesfat");
+		//$sql = 'SELECT adduser($1,$2,$3,$4)';
+		$sql = 'call joinsite($1,$2,$3,$4,$5,$6,$7)';
+		
+		$res = pg_prepare($database->mConnection, "q_joinsite", $sql);
+
+		$res = pg_execute($database->mConnection, "q_joinsite", array($this->mEmail,$this->mPassword, $this->mFirstName, $this->mMiddleName, $this->mLastName, $this->mPhone, $this->mAddress));
+		
+		//$result = pg_prepare($dbconn, "my_query", 'SELECT * FROM shops WHERE name = $1');
+		//$result = pg_prepare($database->mConnection, "q_joinsite", 'joinsite($1,$2,$3,$4,$5,$6,$7)');
+        }
+
 }
 	$join = new Join();	
 
