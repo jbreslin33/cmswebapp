e7f1703c7c0d58474a20b7b712af4562d24d3b2e
Jim Breslin <jbreslin33@gmailcom>
2019-03-03 14:37:47 -0500

    passwords

--- scripts/screens/join_site_screen.js
+++ scripts/screens/join_site_screen.js
@@ -46,16 +46,34 @@ class JoinSiteScreen
 				this.send();
 				console.log('valid');
 				console.log('passwords match');
+          			document.getElementById('password_message_id').style.color = 'green';
+          			document.getElementById('password_message_id').innerHTML = 'passwords are matching';
+				
 			}
 			else
 			{
 				console.log('passwords dont match');
+          			document.getElementById('password_message_id').style.color = 'red';
+          			document.getElementById('password_message_id').innerHTML = 'passwords are not matching';
 			}
 			//also check password match
 			
 			
 		}
 	}
+	/*
+	 
+	   var check = function() {
+      if (document.getElementById('password').value ==
+          document.getElementById('confirm_password').value) {
+          document.getElementById('message').style.color = 'green';
+          document.getElementById('message').innerHTML = 'matching';
+      } else {
+      		document.getElementById('message').style.color = 'red';
+          document.getElementById('message').innerHTML = 'not matching';
+      }
+  }
+  */
 
 	send()
 	{
