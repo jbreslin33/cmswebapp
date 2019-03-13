41b0669cf2da6b37c644ed5e2c7f166d13b5fd15
Jim Breslin <jbreslin33@gmail.com>
2019-03-13 13:40:30 -0400

    jwt

--- php/classes/login/native_login.php
+++ php/classes/login/native_login.php
@@ -15,50 +15,17 @@ class NativeLogin
                 $result = pg_execute($database->mConnection, "f_native_login", array( $_GET['email'] ,$_GET['password']));
 
 		//return to client
-                $return_code = pg_fetch_result($result, 0);
+                $return_value = pg_fetch_result($result, 0);
 
-		if ($return_code < -100  && $return_code > -200)
+		if ($return_value < -100  && $return_code > -200)
 		{
 			echo $return_code;
 		}
 		else
 		{
-/*
-			//jwt code
-			// Create token header as a JSON string
-			$header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
-
-			// Create token payload as a JSON string
-			$payload = json_encode(['user_id' => 123]);
-
-			// Encode Header to Base64Url String
-			$base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
-
-			// Encode Payload to Base64Url String
-			$base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
-
-			// Create Signature Hash
-			$signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, 'abC123!', true);
-
-			// Encode Signature to Base64Url String
-			$base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
-
-			// Create JWT
-			$jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
-
-
-			$userId = 12;
-			$secret = 'sec!ReT423*&';
-			$expiration = time() + 3600;
-			$issuer = 'localhost';
-
-			$token = Token::create($userId, $secret, $expiration, $issuer);
-
-			echo "-100," . $token;
- */
 			//encode
-			$secret = 'sec!ReT423*&';
-			$id = 1313;
+			$secret = 's%%xqc!___bzvReT423*&';
+			$id = $return_value;
 			$encoded_token = array();
 			$encoded_token['id'] = $id;
 
