17300583521e9e59d47e48834e44786a9f5283e3
Jim Breslin <jbreslin33@gmail.com>
2019-03-28 13:57:35 -0400

    cos

--- php/classes/insert/insert_forgot_password.php
+++ php/classes/insert/insert_forgot_password.php
@@ -6,49 +6,20 @@ class InsertForgotPassword
 	function __construct($email) 
 	{
 		$this->mEmail = $email;
-		$txt = 'email:';
-		$txt .= $email;
-		error_log($txt);
 
                 $database = new Database("localhost","cms","postgres","mibesfat");
 
 		$this->mAbsoluteURL = "http://elacore.org/#insert_forgot_password_screen/";
 
-		$txt = "";
-
 		$this->mSelector = bin2hex(random_bytes(8));
 
-		$txt .= $this->mSelector;
-
-		$txt .= "GGGGG";	
-
 		$this->mToken = bin2hex(random_bytes(32));
-
-		$txt .= $this->mToken;
-
-		error_log($txt);
 
 		$this->mUrl = sprintf('%sreset.php?%s', $this->mAbsoluteURL, http_build_query([
     			'selector' => $this->mSelector,
     			'validator' => bin2hex($this->mToken)
 			]));
 
-		// Token expiration
-		// Delete any existing tokens for this user
-		/*
-		$this->db->delete('password_reset', 'email', $this->mEmail);
-
-		// Insert reset token into database
-		$insert = $this->db->insert('password_reset',
-    		array(
-        		'email'     =>  $this->mEmail,
-        		'selector'  =>  $this->mSelector,
-        		'token'     =>  hash('sha256', $this->mToken),
-        		'expires'   =>  $this->mExpires->format('U'),
-    			)
-		);
-		 */
-		//jim db stuff to replace thiers
                 $sql = 'select f_insert_forgot_password($1,$2,$3)';
 
                 $prepare_result = pg_prepare($database->mConnection, "f_insert_forgot_password", $sql);
@@ -58,51 +29,6 @@ class InsertForgotPassword
                 $return_value = pg_fetch_result($result, 0);
 
                 echo $return_value;
-
-
-/*
-$selector = bin2hex(random_bytes(8));
-$token = random_bytes(32);
-
-$url = sprintf('%sreset.php?%s', ABS_URL, http_build_query([
-    'selector' => $selector,
-    'validator' => bin2hex($token)
-]));
-
-// Token expiration
-$expires = new DateTime('NOW');
-$expires->add(new DateInterval('PT01H')); // 1 hour
-
-// Delete any existing tokens for this user
-$this->db->delete('password_reset', 'email', $user->email);
-
-// Insert reset token into database
-$insert = $this->db->insert('password_reset', 
-    array(
-        'email'     =>  $user->email,
-        'selector'  =>  $selector, 
-        'token'     =>  hash('sha256', $token),
-        'expires'   =>  $expires->format('U'),
-    )
-);
- */
-		/*
-
-		$sql = 'select f_insert_club($1,$2,$3)';
-		
-		$prepare_result = pg_prepare($database->mConnection, "f_insert_club", $sql);
-
-		$jwt = $_GET['jwt'];
-		$oneRing = new OneRing();
-                $payload = JWT::decode($jwt, $oneRing->mOneRing);
-		$id = $payload->id;
-
-		$result = pg_execute($database->mConnection, "f_insert_club", array( $_GET['name'] ,$_GET['address'], $id));
-
-               	$return_value = pg_fetch_result($result, 0);
-
-                echo $return_value;
-		 */
         }
 }
 $email = $_GET['email'];
