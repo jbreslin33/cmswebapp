f196f64adc5115a27b75709f5603c14992268b66
Jim Breslin <jbreslin33@gmail.com>
2019-04-04 07:58:35 -0400

    why is php mail stuff getting sent back to client

--- php/classes/insert/insert_forgot_password.php
+++ php/classes/insert/insert_forgot_password.php
@@ -10,43 +10,26 @@ class InsertForgotPassword
 
                 $database = new Database("localhost","cms","postgres","mibesfat");
 
-		//$this->mAbsoluteURL = "http://elacore.org/#update_forgot_password_screen&selector=123&token=123";
 		$this->mAbsoluteURL = "http://elacore.org/#update_forgot_password_screen&";
-
 		$this->mSelector = bin2hex(random_bytes(8));
-
 		$this->mToken = bin2hex(random_bytes(32));
-/*
-		$this->mUrl = sprintf('%s?%s', $this->mAbsoluteURL, http_build_query([
-    			'selector' => $this->mSelector,
-    			'token' => $this->mToken
-			]));
- */
-
-		//$this->mUrl = sprintf('%s%s', $this->mAbsoluteURL, '');
 		
 		$this->mUrl = sprintf('%s%s', $this->mAbsoluteURL, http_build_query([
     			'selector' => $this->mSelector,
     			'token' => $this->mToken
 			]));
 
-		error_log($this->mUrl);
-
                 $sql = 'select f_insert_forgot_password($1,$2,$3)';
-
                 $prepare_result = pg_prepare($database->mConnection, "f_insert_forgot_password", $sql);
-
                 $result = pg_execute($database->mConnection, "f_insert_forgot_password", array( $this->mEmail ,$this->mSelector, $this->mToken));
 
                 $return_value = pg_fetch_result($result, 0);
+		$return_value .= ",";
 
 		error_log($return_value);
                 echo $return_value;
 
-
 		$forgotPassword = new ForgotPassword($this->mEmail, $this->mUrl);
-
-		//$message .= sprintf('<a href="%s">%s</a></p>', $url, $url);
         }
 }
 $email = $_GET['email'];
