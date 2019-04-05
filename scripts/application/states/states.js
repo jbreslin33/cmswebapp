86f1649b075a49716daa77c0f8faf41bb299c05c
Jim Breslin <jbreslin33@gmail.com>
2019-04-05 12:28:03 -0400

    home

--- scripts/application/states/states.js
+++ scripts/application/states/states.js
@@ -316,31 +316,6 @@ class INSERT_FORGOT_PASSWORD_APPLICATION extends State
 		{
 			console.log("INSERT_FORGOT_PASSWORD_APPLICATION: EXECUTE");        
 		}
-			
-		if (app.mInsertForgotPasswordScreen.mHit)
-		{
-			app.mInsertForgotPasswordScreen.showSpinner();
-		}
-             
-		if (app.mInsertForgotPasswordScreen.mData)
-                {
-                        var dataArray = app.mInsertForgotPasswordScreen.mData.split(",");
-                        app.mInsertForgotPasswordScreen.mCode = dataArray[0];
-
-                        if (app.mInsertForgotPasswordScreen.mCode == -100)
-                        {
-                                app.mStateMachine.changeState(app.mMAIN_APPLICATION);
-                                //document.getElementById('insert_forgot_password_screen_name_message_id').innerHTML = 'A link to reset password has been sent to your email.';
-                        }
-                        if (app.mInsertForgotPasswordScreen.mCode == -111)
-                        {
-				app.mInsertForgotPasswordScreen.show();
-                                document.getElementById('insert_forgot_password_screen_name_message_id').style.color = 'red';
-                                document.getElementById('insert_forgot_password_screen_name_message_id').innerHTML = 'Email does not exist. Do you want to create an account with that email?';
-				app.mInsertForgotPasswordScreen.mCode = 0;
-				app.mInsertForgotPasswordScreen.mData = null;
-                        }
-                }
 	}
 
         exit(app)
--- scripts/screens/states/insert_forgot_password_screen_states.js
+++ scripts/screens/states/insert_forgot_password_screen_states.js
@@ -51,6 +51,10 @@ class INIT_INSERT_FORGOT_PASSWORD_SCREEN extends State
                 {
                         console.log("INIT_INSERT_FORGOT_PASSWORD_SCREEN: EXECUTE");
                 }
+                if (owner.mHit)
+                {
+                        owner.mStateMachine.changeState(owner.mWAIT_INSERT_FORGOT_PASSWORD_SCREEN);
+                }
         }
 
         exit(owner)
@@ -61,7 +65,32 @@ class INIT_INSERT_FORGOT_PASSWORD_SCREEN extends State
                 }
         }
 }
+/*
+                if (app.mInsertForgotPasswordScreen.mHit)
+                {
+                        app.mInsertForgotPasswordScreen.showSpinner();
+                }
 
+                if (app.mInsertForgotPasswordScreen.mData)
+                {
+                        var dataArray = app.mInsertForgotPasswordScreen.mData.split(",");
+                        app.mInsertForgotPasswordScreen.mCode = dataArray[0];
+
+                        if (app.mInsertForgotPasswordScreen.mCode == -100)
+                        {
+                                app.mStateMachine.changeState(app.mMAIN_APPLICATION);
+                                //document.getElementById('insert_forgot_password_screen_name_message_id').innerHTML = 'A link to reset password has been sent to your email.';
+                        }
+                        if (app.mInsertForgotPasswordScreen.mCode == -111)
+                        {
+                                app.mInsertForgotPasswordScreen.show();
+                                document.getElementById('insert_forgot_password_screen_name_message_id').style.color = 'red';
+                                document.getElementById('insert_forgot_password_screen_name_message_id').innerHTML = 'Email does not exist. Do you want to create an account with that email?';
+                                app.mInsertForgotPasswordScreen.mCode = 0;
+                                app.mInsertForgotPasswordScreen.mData = null;
+                        }
+                }
+*/
 class WAIT_INSERT_FORGOT_PASSWORD_SCREEN extends State
 {
         constructor()
@@ -82,6 +111,29 @@ class WAIT_INSERT_FORGOT_PASSWORD_SCREEN extends State
                 if (owner.mStateLogs || owner.mStateExecuteLogs)
                 {
                         console.log("WAIT_INSERT_FORGOT_PASSWORD_SCREEN: EXECUTE");
+                }
+                if (owner.mHit)
+                {
+                        owner.showSpinner();
+                }
+
+                if (owner.mData)
+                {
+                        var dataArray = owner.mData.split(",");
+                        owner.mCode = dataArray[0];
+
+                        if (owner.mCode == -100)
+                        {
+                                owner.mApplication.mStateMachine.changeState(owner.mApplication.mMAIN_APPLICATION);
+                        }
+                        if (owner.mCode == -111)
+                        {
+                                owner.show();
+                                document.getElementById('insert_forgot_password_screen_name_message_id').style.color = 'red';
+                                document.getElementById('insert_forgot_password_screen_name_message_id').innerHTML = 'Email does not exist. Do you want to create an account with that email?';
+                                owner.mCode = 0;
+                                owner.mData = null;
+                        }
                 }
         }
 
