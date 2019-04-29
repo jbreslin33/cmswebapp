f4f9da2563a5ef36db77610f0864201b51555c42
Jim Breslin <jbreslin33@gmail.com>
2019-04-28 20:30:18 -0400

    sample query done'

--- database/query.sql
+++ database/query.sql
@@ -3,6 +3,7 @@ select * from club_administrators;
 select * from club_members;
 select * from persons;
 
+select * from clubs;
 
 
-select * from club_administrators join club_members on club_members.id=club_administrators.club_member_id;
+select club_administrators.id from club_administrators join club_members on club_members.id=club_administrators.club_member_id join persons on persons.id=club_members.person_id join users on users.person_id=persons.id join clubs on clubs.id=club_members.club_id where club_id = 1 and users.id = 1;
