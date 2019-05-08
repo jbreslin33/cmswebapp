c9a6880986f71894f362971f06a95c9d08bb5237
Jim Breslin <jbreslin33@gmail.com>
2019-05-08 08:32:29 -0400

    forgot password works

--- php/classes/update/update_forgot_password.php
+++ php/classes/update/update_forgot_password.php
@@ -1,10 +1,10 @@
 <?php 
 include_once(getenv("DOCUMENT_ROOT") . "/php/classes/database/database.php");
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/mail/forgot_password.php");
+//include_once(getenv("DOCUMENT_ROOT") . "/php/classes/mail/forgot_password.php");
 
 class UpdateForgotPassword 
 {
-	function __construct($selector,$token,$password) 
+	function __construct($forgot_password_token,$password) 
 	{
                 $database = new Database("localhost","cms","postgres","mibesfat");
 
@@ -18,9 +18,25 @@ class UpdateForgotPassword
 		error_log($return_value);
 
                 echo $return_value;
+
+                //create mail
+		/*
+                $this->mEmail = $email;
+                $this->mSubject = "Forgot Password Link";
+                $this->mAbsoluteURL = "http://elacore.org/#update_forgot_password&";
+
+                $this->mUrl = sprintf('%s%s', $this->mAbsoluteURL, http_build_query([
+                        'forgot_password_token' => $this->mForgotPasswordToken
+                        ]));
+
+                //send mail
+                $mail = new Mail($this->mEmail, $this->mUrl,$this->mSubject);
+		 */
+
+
         }
 }
-$token = $_GET['forgot_password_token'];
+$forgot_password_token = $_GET['forgot_password_token'];
 $password = $_GET['password'];
 
 $updateForgotPassword = new UpdateForgotPassword($forgot_password_token,$password);	
