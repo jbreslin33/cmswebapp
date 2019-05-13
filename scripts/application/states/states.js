6d895f29bafbe05f613f8ecf27760be451955413
Jim Breslin <jbreslin33@gmail.com>
2019-05-13 14:21:44 -0400

    made state but not in it yet

--- scripts/application/states/states.js
+++ scripts/application/states/states.js
@@ -296,9 +296,83 @@ class INSERT_NATIVE_LOGIN_SCREEN_APPLICATION extends State
 		app.mInsertNativeLoginScreen.hide();
 	}
 }
-/*
-*/
 
+
+
+class INSERT_NATIVE_LOGIN_CLUB_SCREEN_APPLICATION extends State
+{
+	constructor() 
+	{
+		super();
+	}
+
+        enter(app)
+        {
+		if (app.mStateLogs || app.mStateEnterLogs)
+		{
+			console.log("INSERT_NATIVE_LOGIN_CLUB_SCREEN_APPLICATION: ENTER");        
+		}
+		if (app.mInsertNativeLoginClubScreen)
+		{
+			//also maybe some clean up as well, so just leaving this if statement here.
+			app.mInsertNativeLoginClubScreen = new InsertLoginClubScreen(app);
+
+		}
+		else
+		{
+			app.mInsertNativeLoginClubScreen = new InsertLoginClubScreen(app);
+		}
+                document.getElementById("insert_native_login_club_screen_nav_id").className += " active";
+		app.mInsertNativeLoginClubScreen.show();
+	}
+
+        execute(app)
+        {
+		if (app.mStateLogs || app.mStateExecuteLogs)
+		{
+			console.log("INSERT_NATIVE_LOGIN_CLUB_SCREEN_APPLICATION: EXECUTE");        
+		}
+              
+                if (app.mInsertNativeLoginClubScreen.mData)
+                {
+                        var dataArray = app.mInsertNativeLoginClubScreen.mData.split(",");
+                        app.mInsertNativeLoginClubScreen.mCode = dataArray[0];
+
+                        if (app.mInsertNativeLoginClubScreen.mCode == -100)
+                        {
+                                app.mJWT = dataArray[1]; //set jwt
+                                //put in local storage
+                                localStorage.setItem('mJWT', app.mJWT);
+
+                                app.mStateMachine.changeState(app.mMAIN_APPLICATION);
+                        	document.getElementById('insert_native_login_screen_email_message_id').innerHTML = '';
+                        }
+                        if (app.mInsertNativeLoginClubScreen.mCode == -101)
+                        {
+				document.getElementById('insert_native_login_screen_email_message_id').style.color = 'red';
+                        	document.getElementById('insert_native_login_screen_email_message_id').innerHTML = 'Email already exists. Do you want to log in instead?';
+				//show link as well
+				document.getElementById("insert_native_login_screen_link_id").style.display = "block";
+                        }
+                }
+	}
+
+        exit(app)
+        {
+		if (app.mStateLogs || app.mStateExitLogs)
+		{
+			console.log("INSERT_NATIVE_LOGIN_CLUB_SCREEN_APPLICATION: EXIT");        
+		}
+		app.mInsertNativeLoginClubScreen.mCode = 0;
+		app.mInsertNativeLoginClubScreen.mData = null;
+
+		//hide it for now maybe delete later
+		var element = document.getElementById("insert_native_login_club_screen_nav_id");
+                element.className = element.className.replace(/\active\b/g, "");
+
+		app.mInsertNativeLoginClubScreen.hide();
+	}
+}
 
 
 class INSERT_FORGOT_PASSWORD_APPLICATION extends State
--- scripts/screens/insert_native_login_club_screen.js
+++ scripts/screens/insert_native_login_club_screen.js
@@ -4,79 +4,17 @@ class InsertLoginClubScreen extends InsertLoginScreen
 {
 	constructor(application)
 	{
-		this.mApplication = application;
+		super(application);
+		console.log('in const for club');
 
 		location.hash = "insert_native_login_club_screen";
-
-		this.mCode = 0;
-                this.mData = null;
-
-		//sql php vars
-		this.mFirstName = null;
-		this.mMiddleName = null;
-		this.mLastName = null;
-		this.mPhone = null;
-		this.mAddress = null;
-		this.mEmail = null;
-		this.mPassword1 = null;
-		this.mPassword2 = null;
 
 		document.getElementById("insertloginscreenbuttonid").addEventListener("click",this.hit.bind(this));
 	}
 
-	hit()
-	{
-		//get vars
-               	this.mFirstName  = document.getElementById("insert_native_login_screen_first_name_id").value;
-               	this.mMiddleName = document.getElementById("insert_native_login_screen_middle_name_id").value;
-               	this.mLastName   = document.getElementById("insert_native_login_screen_last_name_id").value;
-               	this.mPhone      = document.getElementById("insert_native_login_screen_phone_id").value;
-               	this.mAddress    = document.getElementById("insert_native_login_screen_address_id").value;
-               	this.mEmail    	 = document.getElementById("insert_native_login_screen_email_id").value;
-               	this.mPassword1  = document.getElementById("insert_native_login_screen_password1_id").value;
-               	this.mPassword2  = document.getElementById("insert_native_login_screen_password2_id").value;
-
-		var url = "/php/classes/insert/insert_native_login.php?first_name=" + this.mFirstName + "&middle_name=" + this.mMiddleName + "&last_name=" + this.mLastName + "&phone=" + this.mPhone + "&address=" + this.mAddress + "&email=" + this.mEmail + "&password=" + this.mPassword1; 
-
-                var request = new XMLHttpRequest();
-                request.onreadystatechange = function()
-                {
-                        if (request.readyState === XMLHttpRequest.DONE)
-                        {
-                                if (request.status === 200)
-                                {
-                                        var data = this.responseText;
-                                        if (data)
-                                        {
-						APPLICATION.mInsertNativeLoginScreen.mData = data;
-                                        }
-                                }
-                        }
-                };
-
-		var form = document.getElementById('insert_native_login_screen_form_id');
-		if (form.checkValidity() == true) 
-		{
-			var passwordMatch = false;
-
-			if (this.mPassword1 == this.mPassword2)
-			{
-				request.open('POST', url);
-                		request.send();
-
-          			document.getElementById('password_message_id').style.color = 'green';
-          			document.getElementById('password_message_id').innerHTML = 'passwords are matching';
-			}
-			else
-			{
-          			document.getElementById('password_message_id').style.color = 'red';
-          			document.getElementById('password_message_id').innerHTML = 'passwords are not matching';
-			}
-		}
-	}
-        
 	show()
 	{
+		//also show google
               	document.getElementById("insert_native_login_screen_html_id").style.display = "block";
 		//hide link
 		document.getElementById("insert_native_login_screen_link_id").style.display = "none";
@@ -84,6 +22,7 @@ class InsertLoginClubScreen extends InsertLoginScreen
 
 	hide()
 	{
+		//also hide google
               document.getElementById("insert_native_login_screen_html_id").style.display = "none";
 	}
 }
