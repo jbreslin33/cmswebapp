cd539ae5ffdb7a0444a245c97faf65aee5bf7059
Jim Breslin <jbreslin33@gmail.com>
2019-05-28 14:33:35 -0400

    fixing google login

--- php/classes/login/google_login.php
+++ php/classes/login/google_login.php
@@ -4,9 +4,40 @@ include_once(getenv("DOCUMENT_ROOT") . "/php/classes/jwt/jwt.php");
 
 class GoogleLogin
 {
-        function __construct($email,$google_id,$id_token,$first_name,$last_name,$club_invite_token)
+        function __construct()
         {
-		$id_token = $_GET['id_token'];
+		$email = null;
+		$google_id = null;
+		$id_token = null;
+		$last_name = null;
+		$first_name = null;
+		$club_invite_token = null;
+	
+		if (isset($_GET['email']))
+		{
+			$email = $_GET['email'];
+		}
+		if (isset($_GET['google_id']))
+		{
+			$google_id = $_GET['google_id'];
+		}
+		if (isset($_GET['id_token']))
+		{
+			$id_token = $_GET['id_token'];
+		}
+		if (isset($_GET['first_name']))
+		{
+			$first_name = $_GET['first_name'];
+		}
+		if (isset($_GET['last_name']))
+		{
+			$last_name = $_GET['last_name'];
+		}
+		if (isset($_GET['club_invite_token']))
+		{
+			$club_invite_token = $_GET['club_invite_token'];
+		}
+
                 $database = new Database("localhost","cms","postgres","mibesfat");
 
                 $sql = 'select f_google_login($1,$2,$3,$4,$5,$6)';
@@ -14,7 +45,6 @@ class GoogleLogin
                 $prepare_result = pg_prepare($database->mConnection, "f_google_login", $sql);
 		$txt = "EMAIL:";
 		$txt .= $email;
-		error_log($txt);
 
                 $result = pg_execute($database->mConnection, "f_google_login", array( $email ,$google_id, $id_token, $first_name, $last_name, $club_invite_token));
 
@@ -38,37 +68,5 @@ class GoogleLogin
 
         }
 }
-	$email = null;
-	$google_id = null;
-	$id_token = null;
-	$last_name = null;
-	$first_name = null;
-	$club_invite_token = null;
-	
-	if (isset($_GET['email']))
-	{
-		$email = $_GET['email'];
-	}
-	if (isset($_GET['google_id']))
-	{
-		$google_id = $_GET['google_id'];
-	}
-	if (isset($_GET['id_token']))
-	{
-		$id_token = $_GET['id_token'];
-	}
-	if (isset($_GET['first_name']))
-	{
-		$first_name = $_GET['first_name'];
-	}
-	if (isset($_GET['last_name']))
-	{
-		$last_name = $_GET['last_name'];
-	}
-	if (isset($_GET['club_invite_token']))
-	{
-		$club_invite_token = $_GET['club_invite_token'];
-	}
-
-        $googleLogin = new GoogleLogin($email,$google_id,$id_token,$first_name,$last_name,$club_invite_token);
+        $googleLogin = new GoogleLogin();
 ?>
