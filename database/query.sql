c16a1cd18ddfb040b9cde094bdca316e6600dedf
Jim Breslin <jbreslin33@gmail.com>
2019-05-30 10:25:22 -0400

    home

--- database/query.sql
+++ database/query.sql
@@ -1,3 +1,2 @@
 
-select emails_persons.id as email_person_id, emails_persons_persons.id as email_person_person_id, persons.first_name, persons.last_name from persons join emails_persons on emails_persons.person_id=persons.id left outer join emails_persons_persons on emails_persons_persons.email_person_id=emails_persons.id join emails on emails.id=emails_persons.email_id where emails.id = 1;
-
+select emails_persons.id as email_person_id, emails_persons_persons.id as email_person_person_id, persons.first_name, persons.last_name from persons join emails_persons on emails_persons.person_id=persons.id left outer join emails_persons_persons on emails_persons_persons.email_person_id=emails_persons.id join emails on emails.id=emails_persons.email_id where emails.id = 1; 
