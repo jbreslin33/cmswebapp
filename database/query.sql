da159548c221f78a59e3319133ec9dc2b62b3840
Jim Breslin <jbreslin33@gmail.com>
2019-05-31 12:55:20 -0400

    join

--- database/query.sql
+++ database/query.sql
@@ -7,7 +7,10 @@
 select * from emails_persons union select * from emails_persons_persons;
 
 select persons.id as person_id, emails_persons.id as email_person_id, persons.first_name, persons.last_name from emails_persons join persons on persons.id=emails_persons.person_id;
+select persons.id as person_id, emails_persons_persons.id as email_person_person_id, persons.first_name, persons.last_name from emails_persons_persons join persons on persons.id=emails_persons_persons.person_id;
 
+select persons.id as person_id, emails_persons.id as email_person_id, persons.first_name, persons.last_name from emails_persons join persons on persons.id=emails_persons.person_id
+union
 select persons.id as person_id, emails_persons_persons.id as email_person_person_id, persons.first_name, persons.last_name from emails_persons_persons join persons on persons.id=emails_persons_persons.person_id;
 
 --select emails_persons.id as email_person_id, emails_persons_persons.id as email_person_person_id, persons.first_name, persons.last_name from persons join emails_persons on emails_persons.person_id=persons.id left outer join emails_persons_persons on emails_persons_persons.email_person_id=emails_persons.id join emails on emails.id=emails_persons.email_id where emails.id = 1; 
