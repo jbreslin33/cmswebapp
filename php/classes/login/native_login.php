62ebc10296d9851d3dec2e007d2a97223d625743
Jim Breslin <jbreslin33@gmail.com>
2019-06-04 20:30:49 -0400

    making back to login so native login works, next cosmetic and make main person default but allow for switch in menu

--- php/classes/login/native_login.php
+++ php/classes/login/native_login.php
@@ -19,7 +19,7 @@ class NativeLogin
 		error_log($return_value);
 		$return_value_array = explode(",",$return_value);
 		$email_person_id = array_shift($return_value_array);
-		$data = implode($return_value_array);
+		$data = implode(",",$return_value_array);
 
 		error_log("got this:");
 		error_log($email_person_id);
