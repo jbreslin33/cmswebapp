d0cac67d9deb75eed4b87e4b3dcf8911a311c331
Jim Breslin <jbreslin33@gmail.com>
2019-06-04 20:20:15 -0400

    packet ready to send

--- php/classes/login/native_login.php
+++ php/classes/login/native_login.php
@@ -24,63 +24,37 @@ class NativeLogin
 		error_log("got this:");
 		error_log($email_person_id);
 		error_log($data);
-	}
-/*
-                if ($return_value < -100  && $return_value > -200)
+		
+		if ($email_person_id < -100  && $email_person_id > -200)
                 {
-                        echo $return_value;
+                        echo $email_person_id;
                 }
                 else
 		{
-                	$sqlA = 'select f_native_login_select persons($1)';
-                	$prepare_resultA = pg_prepare($database->mConnection, "f_native_login_select_persons", $sql);
-//			   $result = pg_execute($database->mConnection, "f_google_login", array( $email ,$google_id, $id_token, $first_name, $last_name, $club_invite_token));
-
-                	//$resultA = pg_execute($database->mConnection, "f_native_login_select_persons", array($return_value));
-                	$resultA = pg_execute($database->mConnection, "f_native_login_select_persons", array(1));
-
-			$data = "";
-			$email_person_id = null;
 			$email_person_person_id = null;
-                	while ($row = pg_fetch_row($resultA))
-                	{
-                        	$data .= $row[0];
-                	}
 		
 			if ($data)
 			{
-				$decoded_data = json_decode($data);
+				//encode
+				$oneRing = new OneRing();
+				$encoded_token = array();
+				$encoded_token['email_person_id'] = $email_person_id;
+				$encoded_token['email_person_person_id'] = null;
+				$jwt = JWT::encode($encoded_token, $oneRing->mOneRing);
 
-				if ($data < -101 && $data > -200)
-				{
-					echo $data;
-				}
-				else
-				{
-					//encode
-					$oneRing = new OneRing();
+				$front = '{ "persons" :';
+               			$back = '}';
+               			$return_value = "";
+               			$return_value .= $front;
+               			$return_value .= $data;
+               			$return_value .= $back;
 
-					$encoded_token = array();
-					$encoded_token['email_person_id'] = $decoded_data[0]->email_person_id;
-					$encoded_token['email_person_person_id'] = null;
-
-					$front = '{ "persons" :';
-                			$back = '}';
-
-                			$return_value = "";
-                			$return_value .= $front;
-                			$return_value .= $data;
-                			$return_value .= $back;
-
-					$jwt = JWT::encode($encoded_token, $oneRing->mOneRing);
-					$txt =  "-100," . $jwt . "," . $return_value;
-					error_log($txt);
-					echo $txt;
-				}
+				$txt =  "-100," . $jwt . "," . $return_value;
+				error_log($txt);
+				echo $txt;
 			}
         	}
 	}
- */
 }
         $nativeLogin = new NativeLogin();
 ?>
