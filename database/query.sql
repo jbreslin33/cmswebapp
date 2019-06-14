d836007a86cb1daa305b1ded00db9fcaedf7700f
Jim Breslin <jbreslin33@gmail.com>
2019-06-14 16:31:18 -0400

    clubs

--- database/query.sql
+++ database/query.sql
@@ -1,3 +1,7 @@
 
-select club_administrators.id from club_administrators join club_members on club_members.id=club_administrators.club_member_id where club_members.person_id = 1;
+--select club_administrators.id from club_administrators join club_members on club_members.id=club_administrators.club_member_id where club_members.person_id = 1;
+
+select clubs.id, clubs.name from clubs join club_members on club_members.club_id=clubs.id join persons on persons.id=club_members.person_id where persons.id = 1; 
+
+
 
