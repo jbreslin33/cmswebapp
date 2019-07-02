91bf2ff90c93ac852891900cf0fbbffa4a675087
Jim Breslin <jbreslin33@gmail.com>
2019-07-02 19:48:00 -0400

    query

--- database/query.sql
+++ database/query.sql
@@ -9,6 +9,10 @@
 
 --select clubs.id, clubs.name from clubs join club_members on club_members.club_id=clubs.id join persons on persons.id=club_members.person_id join emails_persons on emails_persons.person_id=persons.id where emails_persons.email_id = 1; 
 
-select practices.id, practices.practice_date from practices join teams on teams.id=practices.team_id;  
+select practices.id, practices.practice_date from practices 
+join teams on teams.id=practices.team_id
+join team_members on team_members.team_id=teams.id
+
+;  
 
 
