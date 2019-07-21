3563a64ee4c47d06127a2add1df8cca9511ef51d
Jim Breslin <jbreslin33@gmail.com>
2019-07-21 14:23:18 -0400

    insert club using screen parent

--- php/classes/insert/insert_club.php
+++ php/classes/insert/insert_club.php
@@ -1,33 +1,22 @@
 <?php 
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/database/database.php");
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/jwt/jwt.php");
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/onering/onering.php");
+include_once(getenv("DOCUMENT_ROOT") . "/php/classes/screens/screen.php");
 
-class InsertClub 
+class InsertClub extends Screen
 {
 	function __construct() 
 	{
-                $database = new Database("localhost","cms","postgres","mibesfat");
-
-		//actually we are going to get the jwt and need to extract id
-
-		$sql = 'select f_insert_club($1,$2,$3,$4)';
-		
-		$prepare_result = pg_prepare($database->mConnection, "f_insert_club", $sql);
-
-		$jwt = $_GET['jwt'];
-		$oneRing = new OneRing();
-                $payload = JWT::decode($jwt, $oneRing->mOneRing);
-		$email_id = $payload->email_id;
-
-		$result = pg_execute($database->mConnection, "f_insert_club", array( $_GET['name'] ,$_GET['address'], $email_id, $_GET['person_id']));
-
-               	$return_value = pg_fetch_result($result, 0);
-
-		$result_set = $database->formatResultSet($return_value);
-		error_log($result_set);
-                echo $result_set;
+		parent::__construct();
         }
+
+	function getResult()
+	{
+                $database = new Database("localhost","cms","postgres","mibesfat");
+		$sql = 'select f_insert_club($1,$2,$3,$4)';
+		$prepare_result = pg_prepare($database->mConnection, "f_insert_club", $sql);
+		$result = pg_execute($database->mConnection, "f_insert_club", array( $_GET['name'] ,$_GET['address'], $this->getSenderEmailId(), $_GET['person_id']));
+
+                return pg_fetch_result($result, 0);
+	}
 }
 
 $insertClub = new InsertClub();	
