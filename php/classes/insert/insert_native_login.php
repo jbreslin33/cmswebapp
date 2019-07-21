46f20ff86408aafc2a6b3c151288ed4aceab6d33
Jim Breslin <jbreslin33@gmail.com>
2019-07-21 16:27:45 -0400

    actual native login earlier change was for native club login

--- php/classes/insert/insert_native_login.php
+++ php/classes/insert/insert_native_login.php
@@ -1,25 +1,22 @@
 <?php 
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/database/database.php");
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/jwt/jwt.php");
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/onering/onering.php");
+include_once(getenv("DOCUMENT_ROOT") . "/php/classes/screens/screen.php");
 
-class InsertNativeLogin 
+class InsertNativeLogin extends Screen 
 {
 	function __construct() 
 	{
-                $database = new Database("localhost","cms","postgres","mibesfat");
-
+		parent::__construct();	
+	}
+		
+	function getResult()
+	{
 		$sql = 'select f_insert_native_login($1,$2,$3,$4,$5,$6,$7)';
-		
-		$prepare_result = pg_prepare($database->mConnection, "f_insert_native_login", $sql);
-		
-		$result = pg_execute($database->mConnection, "f_insert_native_login", array( $_GET['email'] , $_GET['password'], $_GET['first_name'], $_GET['middle_name'], $_GET['last_name'], $_GET['phone'], $_GET['address']));
-
-		$return_value = pg_fetch_result($result, 0); 
-
-		$result_set = $database->formatResultSet($return_value);
-		echo $result_set;
-        }
+		$prepare_result = pg_prepare($this->mDatabase->mConnection, "f_insert_native_login", $sql);
+		$result = pg_execute($this->mDatabase->mConnection, "f_insert_native_login", array( $_GET['email'] , $_GET['password'], $_GET['first_name'], $_GET['middle_name'], $_GET['last_name'], $_GET['phone'], $_GET['address']));
+                return pg_fetch_result($result, 0);
+	}
 }
-	$insertNativeLogin = new InsertNativeLogin();	
+
+$insertNativeLogin = new InsertNativeLogin();	
+
 ?>
