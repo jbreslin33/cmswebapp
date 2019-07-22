5ea9369dc784462f3b46754fe3690702eccc5fe4
Jim Breslin <jbreslin33@gmail.com>
2019-07-21 22:09:41 -0400

    delete person works as well but not thrileed at states

--- scripts/screens/delete_person_screen.js
+++ scripts/screens/delete_person_screen.js
@@ -19,16 +19,48 @@ class DeletePersonScreen extends Screen
 
 	get()
 	{
+		console.log('get is called in delete');
 		APPLICATION.getCurrentScreen().setUrl("/php/classes/select/select_persons.php?jwt=" + APPLICATION.getJWT()); 
                 APPLICATION.getCurrentScreen().ajax();
 	}
 
+
 	hit()
 	{
+		this.mHit = true;
+
 		var select = document.getElementById("delete_person_screen_select_id");
                 var person_id = select.options[select.selectedIndex].value;
 		
 		APPLICATION.getCurrentScreen().setUrl("/php/classes/screens/delete_person.php?name=" + "&jwt=" + APPLICATION.getJWT() + '&person_id=' + person_id);
                 APPLICATION.getCurrentScreen().ajax();
 	}
+
+        execute()
+        {
+                this.processData();
+
+                if (this.mJson)
+                {
+                        if (this.mJson.persons)
+                        {
+				console.log('fill select');
+
+                                //this.mApplication.mStateMachine.changeState(this.mApplication.mMAIN_APPLICATION);
+                        }
+                }
+
+                if (this.mJson && this.mHit)
+                {
+                        if (this.mJson.persons)
+                        {
+                                this.mApplication.mStateMachine.changeState(this.mApplication.mMAIN_APPLICATION);
+                        }
+                }
+               
+		this.mCode = 0;
+                this.mData = null;
+                this.mJson = null;
+
+        }
 }
