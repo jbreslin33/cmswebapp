ae4abb736e2809e89fa30ec2505abbd4f1a322d8
Jim Breslin <jbreslin33@gmail.com>
2019-07-23 17:50:52 -0400

    error in php and also getting token back on sending email

--- php/classes/screens/update_forgot_password.php
+++ php/classes/screens/update_forgot_password.php
@@ -1,67 +1,37 @@
 <?php 
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/database/database.php");
+include_once(getenv("DOCUMENT_ROOT") . "/php/classes/screens/screen.php");
 include_once(getenv("DOCUMENT_ROOT") . "/php/classes/mail/mail.php");
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/jwt/jwt.php");
-include_once(getenv("DOCUMENT_ROOT") . "/php/classes/onering/onering.php");
 
-class UpdateForgotPassword 
+class UpdateForgotPassword extends Screen
 {
 	function __construct($forgot_password_token,$password) 
 	{
-                $database = new Database("localhost","cms","postgres","mibesfat");
-		error_log($forgot_password_token);
-		error_log($password);
+
+		parent::__construct();
+	}
+
+	getResult()
+	{
+		$forgot_password_token = $_GET['forgot_password_token'];
+		$password = $_GET['password'];
+		//$email = $_GET['email'];
+		$email = "jbreslin33@yahoo.com";
+		
+		//mail that you changed password
+               	$this->mEmail = $email;
+               	$this->mSubject = "Password has been changed.";
+               	$this->mURL = "http://elacore.org/#insert_forgot_password_screen";
+		$this->mBody = "You have recently changed your password. If this was not you. Click here: ";
+		$this->mBody .= $this->mURL;
 
                 $sql = 'select f_update_forgot_password($1,$2)';
-
                 $prepare_result = pg_prepare($database->mConnection, "f_update_forgot_password", $sql);
-
                 $result = pg_execute($database->mConnection, "f_update_forgot_password", array( $forgot_password_token, $password));
+		
+		$mail = new Mail($this->mEmail,$this->mSubject,$this->mBody);
 
-                $return_value = pg_fetch_result($result, 0);
-
-                if ($return_value < -100  && $return_value > -200)
-		{
-                	echo $return_value;
-		}
-		else
-		{
-                       	$return_value_array = explode(",",$return_value);
-                        $email = array_shift($return_value_array);
-                        $email_person_id = array_shift($return_value_array);
-                        $data = implode(",",$return_value_array);
-
-                        if ($data)
-                        {
-				//mail that you changed password
-                		$this->mEmail = $email;
-                		$this->mSubject = "Password has been changed.";
-                		$this->mURL = "http://elacore.org/#insert_forgot_password_screen";
-				$this->mBody = "You have recently changed your password. If this was not you. Click here: ";
-				$this->mBody .= $this->mURL;
-                		$mail = new Mail($this->mEmail,$this->mSubject,$this->mBody);
-
-                                //encode
-                                $oneRing = new OneRing();
-                                $encoded_token = array();
-                                $encoded_token['email_person_id'] = $email_person_id;
-                                $jwt = JWT::encode($encoded_token, $oneRing->mOneRing);
-
-                                $front = '{ "persons" :';
-                                $back = '}';
-                                $return_value = "";
-                                $return_value .= $front;
-                                $return_value .= $data;
-                                $return_value .= $back;
-
-                                $txt =  "-100," . $jwt . "," . $return_value;
-                                echo $txt;
-                        }
-		}
-        }
+                return pg_fetch_result($result, 0);
+	}
 }
-$forgot_password_token = $_GET['forgot_password_token'];
-$password = $_GET['password'];
-
-$updateForgotPassword = new UpdateForgotPassword($forgot_password_token,$password);	
+$updateForgotPassword = new UpdateForgotPassword();	
 ?>
