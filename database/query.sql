454469fa31bc67d69ceab3e4521b5bd228e79dce
Jim Breslin <jbreslin33@gmail.com>
2019-08-06 10:39:01 -0400

    query works

--- database/query.sql
+++ database/query.sql
@@ -2,16 +2,15 @@
                 from practices
 
                 join teams on teams.id=practices.team_id
-
                 join team_club_players on team_club_players.team_id=teams.id
                 join club_players on club_players.id=team_club_players.club_player_id
                 join players on players.id=club_players.player_id
                 join persons on persons.id=players.person_id
                 join pitches on pitches.club_id=teams.club_id
 
-                join emails_persons on emails_persons.person_id=persons.id;
+                join emails_persons on emails_persons.person_id=persons.id
 
-                --where emails_persons.email_id = 1 AND practices.event_date > now() - interval '1 day';
+                where emails_persons.email_id = 1 AND practices.event_date > now() - interval '1 day';
 
 
  select practices.id, practices.event_date, practices.arrival_time, practices.start_time, practices.end_time, practices.address, practices.coordinates, pitches.name as pitch_name, practices.field_name, teams.name as team_name
