5664a301ddbb212d777828aaacc321f20d92339f
Jim Breslin <jbreslin33@gmail.com>
2019-08-12 17:12:29 -0400

    nacho

--- database/build.sql
+++ database/build.sql
@@ -2391,11 +2391,53 @@ BEGIN
         insert into team_club_players (team_club_person_id, club_player_id) values (returning_team_club_person_id, returning_club_player_id);
 
 	--------------------------------------Nacho Obando
+	--EMAILS
 	--Nacho Obando
-	insert into emails (email) values ('Maxwell.obando27@gmail.com');
+	insert into emails (email) values ('Maxwell.obando27@gmail.com') returning id into returning_email_id_player_a;
 
 	--Michelle Obando
-	insert into emails (email) values ('Missyobando50@gmail.com');
+	insert into emails (email) values ('Missyobando50@gmail.com') returning id into returning_email_id_mother;
+
+       	--PERSONS	
+	insert into persons (first_name, middle_name, last_name, phones, address) values ('Nacho', null, 'Obando', null, null) returning id into returning_person_id_player_a;
+        insert into persons (first_name, middle_name, last_name, phones, address) values ('Louis', null, 'Obando', null, null) returning id into returning_person_id_father;
+        insert into persons (first_name, middle_name, last_name, phones, address) values ('Michelle', null, 'Obando', null, null) returning id into returning_person_id_mother;
+
+        --EMAILS_PERSONS
+        insert into emails_persons (email_id, person_id) values (returning_email_id_player_a, returning_person_id_player_a);
+        insert into emails_persons (email_id, person_id) values (returning_email_id_player_a, returning_person_id_father);
+        insert into emails_persons (email_id, person_id) values (returning_email_id_player_a, returning_person_id_mother);
+	
+	insert into emails_persons (email_id, person_id) values (returning_email_id_father_a, returning_person_id_player_a);
+        insert into emails_persons (email_id, person_id) values (returning_email_id_father_a, returning_person_id_father);
+        insert into emails_persons (email_id, person_id) values (returning_email_id_father_a, returning_person_id_mother);
+        
+	insert into emails_persons (email_id, person_id) values (returning_email_id_mother, returning_person_id_player_a);
+        insert into emails_persons (email_id, person_id) values (returning_email_id_mother, returning_person_id_father);
+        insert into emails_persons (email_id, person_id) values (returning_email_id_mother, returning_person_id_mother);
+
+	--CLUB_EMAILS
+        insert into club_emails (club_id, email_id) values ($1,returning_email_id_player_a);
+        insert into club_emails (club_id, email_id) values ($1,returning_email_id_mother);
+	
+	--CLUB_PERSONS
+        insert into club_persons (club_id, person_id) values ($1, returning_person_id_player_a) returning id into returning_club_person_id_player_a;
+        insert into club_persons (club_id, person_id) values ($1, returning_person_id_father) returning id into returning_club_person_id_father;
+        insert into club_persons (club_id, person_id) values ($1, returning_person_id_mother) returning id into returning_club_person_id_mother;
+	
+	--PLAYER
+        insert into dobs (dob) values ('2004-12-27') returning id into returning_dob_id;
+        insert into players (dob_id,person_id) values (returning_dob_id,returning_person_id_player_a) returning id into returning_player_id;
+
+        --CLUB_PLAYERS
+        insert into club_players (club_person_id, player_id, uniform_number) values (returning_club_person_id_player_a, returning_player_id, 27) returning id into returning_club_player_id;
+
+        --TEAM_CLUB_PERSONS
+        insert into team_club_persons (club_person_id, team_id) values (returning_club_person_id_player_a, returning_team_id) returning id into returning_team_club_person_id;
+
+        --TEAM_CLUB_PLAYERS
+        insert into team_club_players (team_club_person_id, club_player_id) values (returning_team_club_person_id, returning_club_player_id);
+
 
 	----------------------------------Tedi Shaho
 	--Tedi Shaho
