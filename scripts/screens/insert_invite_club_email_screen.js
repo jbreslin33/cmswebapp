6768e5fefd6b81402c35c6528db74fc43eb0da1b
Jim Breslin <jbreslin33@gmail.com>
2019-11-11 12:21:07 -0500

    check on php insert_invite_club_email as it should be inserting into db. It is sending email

--- scripts/screens/insert_invite_club_email_screen.js
+++ scripts/screens/insert_invite_club_email_screen.js
@@ -15,29 +15,14 @@ class InsertInviteClubEmailScreen extends Screen
                 this.setMessageElement(document.getElementById("insert_invite_club_email_screen_message_id"));
                 this.setForm(document.getElementById("insert_invite_club_email_screen_form_id"));
                 this.setSpinner(document.getElementById("insert_invite_club_email_screen_spinner_id"));
-							 //insert_invite_club_email_screen_spinner_id
 	}
 	
 	hit()
 	{
-		this.mHit = true;
-
       		var email  = document.getElementById("insert_invite_club_email_screen_email_id").value;
-                var person_select = document.getElementById("person_select_id");
-                var club_select = document.getElementById("club_select_id");
 
-                if (club_select.length > 0 && person_select.length > 0)
-                {
-                        var club_id = club_select.options[club_select.selectedIndex].value;
-                        var person_id = person_select.options[person_select.selectedIndex].value;
+                APPLICATION.getCurrentScreen().setUrl("/php/classes/screens/insert_invite_club_email.php?jwt=" + APPLICATION.getJWT() + this.getParameters() + '&email=' + email);
 
-                        APPLICATION.getCurrentScreen().setUrl("/php/classes/screens/insert_invite_club_email.php?jwt=" + APPLICATION.getJWT() + '&club_id=' + club_id + '&person_id=' + person_id + '&email=' + email);
-
-                        APPLICATION.getCurrentScreen().ajax();
-                }
-                else
-                {
-                        this.setMessage("You must select a club first","red");
-                }
+                APPLICATION.getCurrentScreen().ajax();
 	}
 }
