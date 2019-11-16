22ee958c872b0d5fe18ca44bcd87b8c03238b9b7
Jim Breslin <jbreslin33@gmail.com>
2019-11-16 18:35:14 -0500

    insert person not inserting emails_persons on join path

--- database/build.sql
+++ database/build.sql
@@ -1860,15 +1860,35 @@ CREATE OR REPLACE PROCEDURE p_insert_person(first_name TEXT, middle_name TEXT, l
 LANGUAGE plpgsql
 AS $$
 DECLARE
-        returning_person_id integer;
+        found_emails_persons_id integer;
 	rec RECORD;
 BEGIN
-        insert into persons (first_name, middle_name, last_name, phones, address) values (first_name, middle_name, last_name, ARRAY [phones], address) returning id into returning_person_id;
-	FOR rec IN
-		select distinct email_id from emails_persons where person_id = $7
-	LOOP
-		insert into emails_persons (email_id, person_id) values (rec.email_id, returning_person_id) returning id into x; 
-	END LOOP;
+        insert into persons (first_name, middle_name, last_name, phones, address) values (first_name, middle_name, last_name, ARRAY [phones], address) returning id into x;
+	select id into found_emails_persons_id from emails_persons where person_id = x;
+
+	IF $7 > 0 THEN
+		--new one for brand new unaffilated persons
+		IF found_emails_persons_id > 0 THEN
+			--do nothing		
+		ELSE
+			--insert an emails_persons
+			insert into emails_persons (email_id, person_id) values ($6,x);
+		END IF;
+
+		FOR rec IN
+			select distinct email_id from emails_persons where person_id = $7
+		LOOP
+			insert into emails_persons (email_id, person_id) values (rec.email_id, x); 
+		END LOOP;
+
+	ELSE
+		--old one
+		FOR rec IN
+			select distinct email_id from emails_persons where person_id = $7
+		LOOP
+			insert into emails_persons (email_id, person_id) values (rec.email_id, x); 
+		END LOOP;
+	END IF;
 END;
 
 $$;
