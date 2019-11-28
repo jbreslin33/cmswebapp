d71ae50546eed0a8742b39f4abb4429eb17eb77d
Jim Breslin <jbreslin33@gmail.com>
2019-11-27 21:11:12 -0500

    query

--- database/query.sql
+++ database/query.sql
@@ -3,10 +3,13 @@ select * from persons;
                 select distinct clubs.id, clubs.name from clubs
                 join club_persons on club_persons.club_id=clubs.id
                 join club_administrators on club_administrators.club_person_id=club_persons.id
-                where club_persons.person_id = 27 
+                where club_persons.person_id = 27;
 
                 select distinct clubs.id, clubs.name from clubs
                 join club_persons on club_persons.club_id=clubs.id
-                join club_administrators on club_administrators.club_person_id=club_persons.id
-                where club_persons.person_id = 27 
+		join team_club_persons on team_club_persons.club_person_id=club_persons.id
+		join team_club_persons_club_managers on team_club_persons_club_managers.team_club_person_id=team_club_persons.id
+                where club_persons.person_id = 27; 
+
+		select * from team_club_persons_club_managers;
 
