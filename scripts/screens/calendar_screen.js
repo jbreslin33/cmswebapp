950aa9e8e08ee300f38d13081271e752ecb4fe3c
Jim Breslin <jbreslin33@gmail.com>
2020-01-01 21:10:36 -0500

    width is good what up with all the rows?

--- scripts/screens/calendar_screen.js
+++ scripts/screens/calendar_screen.js
@@ -35,7 +35,36 @@ class CalendarScreen extends Screen
 
 		p.innerHTML = calendar.mMonthArray[month] + ' weeks: ' + this.mWeekCount;  
 
-		//make a table
+		//append to table
+		var table = document.getElementById("calendar_table_id");
+
+
+
+		// Create an empty <tr> element and add it to the 1st position of the table:
+		var row = table.insertRow(1);
+		
+		for (i = 0; i < this.mWeekCount; i++)
+		{
+			var tr = table.insertRow(parseInt(i + 1));
+			for (y = 0; y < 7; y++)
+			{
+				var td = tr.insertCell(y);
+				td.innerHTML = "y";
+			}
+			
+		}
+
+		// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+		/*
+		var cell1 = row.insertCell(0);
+		var cell2 = row.insertCell(1);
+
+		// Add some text to the new cells:
+		cell1.innerHTML = "NEW CELL1";
+		cell2.innerHTML = "NEW CELL2";
+		*/
+		
+		
 		/*
                 var div = document.createElement('div');
                 div.setAttribute('class','card');
