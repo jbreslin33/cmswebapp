bfb56bc1ae8350322103e6f73bb362de6886ce41
Jim Breslin <jbreslin33@gmail.com>
2020-01-29 08:53:46 -0500

    build

---
+++ database/build/build.sh
@@ -0,0 +1,3 @@
+sudo -u postgres psql -d footballhome -f database/build/tables.sql
+sudo -u postgres psql -d footballhome -f database/build/procedures.sql
+sudo -u postgres psql -d footballhome -f database/build/celta.sql
--- database/build/procedures.sh
+++ database/build/procedures.sh
@@ -1,1 +1,1 @@
-sudo -u postgres psql -d footballhome -f database/build/tables.sql
+sudo -u postgres psql -d footballhome -f database/build/procedures.sql
