331e5ec70ba03a819c092c9d71bcba15cf03cc70
Jim Breslin <jbreslin33@gmail.com>
2020-02-01 15:14:19 -0500

    query

--- database/query.sql
+++ database/query.sql
@@ -33,3 +33,10 @@ select practices.id, practices.event_date, practices.arrival_time, practices.sta
 
                 where emails_persons.email_id = 24;
 
+               
+		select distinct clubs.id, clubs.name from clubs
+                join club_persons on club_persons.club_id=clubs.id
+                join team_club_persons on team_club_persons.club_person_id=club_persons.id
+                join team_club_persons_club_managers on team_club_persons_club_managers.team_club_person_id=team_club_persons.id
+                where club_persons.person_id = 28; 
+
