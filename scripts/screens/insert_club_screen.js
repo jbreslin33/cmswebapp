99cee775af3bb3a8c0997e0deb06574d51e54407
Jim Breslin <jbreslin33@gmail.com>
2020-02-10 09:16:14 -0500

    insert club

--- scripts/screens/insert_club_screen.js
+++ scripts/screens/insert_club_screen.js
@@ -8,26 +8,30 @@ class InsertClubScreen extends Screen
 
 		location.hash = 'insert_club_screen';
 
-		document.getElementById("addclubscreenbuttonid").onclick = this.hit.bind(this);
-
           	this.setHtml(document.getElementById("insert_club_screen_html_id"));
                 this.setMessageElement(document.getElementById("insert_club_screen_message_id"));
           	this.setForm(document.getElementById("insert_club_screen_form_id"));
           	this.setSpinner(document.getElementById("insert_club_screen_spinner_id"));
+
+               	this.getForm().addEventListener('submit', function(e)
+                {
+                        e.preventDefault();
+                        APPLICATION.getCurrentScreen().hit();
+                });
 	}
 
 	hit()
 	{
       		var name  = document.getElementById("insert_club_screen_name_id").value;
                	var address = document.getElementById("insert_club_screen_address_id").value;
-		if (this.getPersonId() > 0)
-		{
+		//if (this.getPersonId() > 0)
+		//{
 			APPLICATION.getCurrentScreen().setUrl("/php/classes/screens/insert_club.php?" + this.getStandardParameters() + "&name=" + name + "&address=" + address); 
 			APPLICATION.getCurrentScreen().ajax();
-		}
-		else
-		{
+		//}
+		//else
+		//{
 			this.setMessage('You need to select a person. If there are no persons yet you need to create one.','red');
-		}
+		//}
 	}
 }
