087b555e7c153182f35402342ae620cb2eb70bac
Jim Breslin <jbreslin33@gmail.com>
2020-02-28 08:41:17 -0500

    delete not working on avail

--- database/build/procedures.sql
+++ database/build/procedures.sql
@@ -1269,8 +1269,22 @@ BEGIN
 			delete from club_administrators where club_person_id = recF.id; 	
         END LOOP;
 
-	
-	
+--not deleteing below
+
+	--delete from practices_players_availability using team_club_persons, club_persons where club_persons.club_id = $1;
+	FOR recI IN
+		select id from club_persons where club_id = $1
+	LOOP
+		FOR recJ IN
+			select id from club_players where club_person_id = recI.id
+		LOOP
+			FOR recK IN
+				select id from team_club_persons_club_players where club_player_id = recJ.id
+			LOOP
+				delete from practices_players_availability where team_club_persons_club_players_id = recK.id;		
+			END LOOP;
+		END LOOP;			
+	END LOOP;	
 
 	--delete from team_club_persons_club_players using team_club_persons, club_persons where club_persons.club_id = $1;
         FOR recG IN
@@ -1285,8 +1299,6 @@ BEGIN
 
 
 
-	--delete from practices_players_availability using team_club_persons, club_persons where club_persons.club_id = $1;
-	
 	--delete from practices using practice, teams  where teams.club_id = $1;
 	
 	--delete from practice using teams where teams.club_id = $1;
