d79910ed9c7e59dd90d2a511017c79b09ee7ff52
Jim Breslin <jbreslin33@gmail.com>
2020-03-24 19:35:16 -0400

    club profile

--- database/build/procedures.sql
+++ database/build/procedures.sql
@@ -1130,6 +1130,40 @@ BEGIN
 
 END;
 $$;
+
+CREATE OR REPLACE FUNCTION f_club_profile(email_id_p int, person_id_p int, club_id_p int)
+RETURNS text AS $$
+DECLARE
+        result_set text;
+	found_club_administrator_id club_administrators.id%TYPE;
+
+BEGIN
+	select club_administrators.id into found_club_administrator_id from club_administrators join club_persons on club_persons.id=club_administrators.club_person_id where club_persons.person_id = $2;
+	IF found_club_administrator_id > 0 THEN
+	        result_set = CONCAT
+        	(
+                	j_select_persons(email_id_p),
+                	',',
+                	j_select_messages(null),
+                	',',
+                	j_select_codes(-102),
+                	',',
+                	j_select_club_profiles(person_id_p, club_id_p)
+        	);
+	ELSE
+	        result_set = CONCAT
+        	(
+                	j_select_persons($1),
+                	',',
+                	j_select_messages('You are not a club administrator.'),
+                	',',
+                	j_select_codes(-101)
+        	);
+
+	END IF;
+RETURN result_set;
+END;
+$$ LANGUAGE plpgsql;
 --END UPDATE CLUB PROFILE
 
 --BEGIN INSERT PERSON
@@ -1378,6 +1412,182 @@ RETURN result_set;
 END;
 $$ LANGUAGE plpgsql;
 
+--UPDATE PROFILES
+CREATE OR REPLACE PROCEDURE p_update_profile(int,int,int,INOUT x int)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+        ids INT[];
+	found_person_id persons.id%TYPE;
+BEGIN
+
+	IF $3 = 2 THEN
+
+		IF $2 = 1 THEN
+			select person_id into found_person_id from players where person_id = $1; 
+			IF found_person_id > 0  THEN
+				-- DO NOTHING
+			ELSE
+				insert into players (person_id) values ($1) returning id into x;
+			END IF;
+		ELSE
+			--DO NOTHING
+		END IF;
+
+
+		IF $2 = 2 THEN
+			select person_id into found_person_id from parents where person_id = $1; 
+			IF found_person_id > 0  THEN
+				-- DO NOTHING
+			ELSE
+				insert into parents (person_id) values ($1) returning id into x;
+			END IF;
+		ELSE
+			--DO NOTHING
+		END IF;
+
+	
+		IF $2 = 3 THEN
+			--RAISE LOG 'log message in 3 %', $1;
+			select person_id into found_person_id from coaches where person_id = $1; 
+			IF found_person_id > 0  THEN
+				-- DO NOTHING
+			ELSE
+				insert into coaches (person_id) values ($1) returning id into x;
+			END IF;
+		ELSE
+			--DO NOTHING
+		END IF;
+
+
+		IF $2 = 4 THEN
+			select person_id into found_person_id from managers where person_id = $1; 
+			IF found_person_id > 0  THEN
+				-- DO NOTHING
+			ELSE
+				insert into managers (person_id) values ($1) returning id into x;
+			END IF;
+		ELSE
+			--DO NOTHING
+		END IF;
+	
+		IF $2 = 5 THEN
+			select person_id into found_person_id from administrators where person_id = $1; 
+			IF found_person_id > 0  THEN
+				-- DO NOTHING
+			ELSE
+				insert into administrators (person_id) values ($1) returning id into x;
+			END IF;
+		ELSE
+			--DO NOTHING
+		END IF;
+
+	ELSE
+		IF $2 = 1 THEN
+			delete from players where person_id = $1 returning id into x;  
+		ELSE
+			--DO NOTHING
+		END IF;
+
+		IF $2 = 2 THEN
+			delete from parents where person_id = $1 returning id into x; 
+		ELSE
+			--DO NOTHING
+		END IF;
+
+		IF $2 = 3 THEN
+			delete from coaches where person_id = $1 returning id into x; 
+		ELSE
+			--DO NOTHING
+		END IF;
+
+		IF $2 = 4 THEN
+			delete from managers where person_id = $1 returning id into x; 
+		ELSE
+			--DO NOTHING
+		END IF;
+
+		IF $2 = 5 THEN
+			delete from administrators where person_id = $1 returning id into x; 
+		ELSE
+			--DO NOTHING
+		END IF;
+		RAISE LOG 'log message x:%', x;
+
+	END IF;
+
+EXCEPTION
+	WHEN others THEN
+        x := -100;
+	
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION f_update_profile(int,int,int,int)
+RETURNS text AS $$
+DECLARE
+        result_set text;
+        DECLARE x int := -111;
+        json_result text;
+BEGIN
+
+        IF $2 is NULL THEN
+        ELSE
+
+                CALL p_update_profile($2,$3,$4,x);
+
+                IF x > 0 THEN
+                        result_set = CONCAT
+                        (
+                                j_select_persons($1),
+                                ',',
+                                j_select_messages('Profile Updated.'),
+                                ',',
+                                j_select_codes(-101),
+                                ',',
+				j_select_profiles($2)
+                        );
+
+                ELSE
+                        result_set = CONCAT
+                        (
+                                j_select_persons($1),
+                                ',',
+                                j_select_messages('Something went wrong with updating profile. Please try again.'),
+                                ',',
+                                j_select_codes(-101),
+                                ',',
+				j_select_profiles($2)
+                        );
+
+                END IF;
+        END IF;
+
+RETURN result_set;
+END;
+$$ LANGUAGE plpgsql;
+
+--BEGIN SELECT PROFILES
+CREATE OR REPLACE FUNCTION f_profile(email_id_p int, person_id_p int)
+RETURNS text AS $$
+DECLARE
+        result_set text;
+BEGIN
+        result_set = CONCAT
+	(
+		j_select_persons($1),
+		',',
+		j_select_messages(null),
+		',',
+		j_select_codes(-102),
+		',',
+		j_select_profiles($2)
+	);
+RETURN result_set;
+END;
+$$ LANGUAGE plpgsql;
+
+--END UPDATE PROFILE
 -------------------------------------------------------------------------------
 
 
@@ -1853,218 +2063,9 @@ $$;
 
 
 
---BEGIN UPDATE PROFILE
-CREATE OR REPLACE PROCEDURE p_update_profile(int,int,int,INOUT x int)
-LANGUAGE plpgsql
-AS $$
-DECLARE
-        ids INT[];
-	found_person_id persons.id%TYPE;
-BEGIN
-
-	IF $3 = 2 THEN
-
-		IF $2 = 1 THEN
-			select person_id into found_person_id from players where person_id = $1; 
-			IF found_person_id > 0  THEN
-				-- DO NOTHING
-			ELSE
-				insert into players (person_id) values ($1) returning id into x;
-			END IF;
-		ELSE
-			--DO NOTHING
-		END IF;
 
 
-		IF $2 = 2 THEN
-			select person_id into found_person_id from parents where person_id = $1; 
-			IF found_person_id > 0  THEN
-				-- DO NOTHING
-			ELSE
-				insert into parents (person_id) values ($1) returning id into x;
-			END IF;
-		ELSE
-			--DO NOTHING
-		END IF;
 
-	
-		IF $2 = 3 THEN
-			--RAISE LOG 'log message in 3 %', $1;
-			select person_id into found_person_id from coaches where person_id = $1; 
-			IF found_person_id > 0  THEN
-				-- DO NOTHING
-			ELSE
-				insert into coaches (person_id) values ($1) returning id into x;
-			END IF;
-		ELSE
-			--DO NOTHING
-		END IF;
-
-
-		IF $2 = 4 THEN
-			select person_id into found_person_id from managers where person_id = $1; 
-			IF found_person_id > 0  THEN
-				-- DO NOTHING
-			ELSE
-				insert into managers (person_id) values ($1) returning id into x;
-			END IF;
-		ELSE
-			--DO NOTHING
-		END IF;
-	
-		IF $2 = 5 THEN
-			select person_id into found_person_id from administrators where person_id = $1; 
-			IF found_person_id > 0  THEN
-				-- DO NOTHING
-			ELSE
-				insert into administrators (person_id) values ($1) returning id into x;
-			END IF;
-		ELSE
-			--DO NOTHING
-		END IF;
-
-	ELSE
-		IF $2 = 1 THEN
-			delete from players where person_id = $1 returning id into x;  
-		ELSE
-			--DO NOTHING
-		END IF;
-
-		IF $2 = 2 THEN
-			delete from parents where person_id = $1 returning id into x; 
-		ELSE
-			--DO NOTHING
-		END IF;
-
-		IF $2 = 3 THEN
-			delete from coaches where person_id = $1 returning id into x; 
-		ELSE
-			--DO NOTHING
-		END IF;
-
-		IF $2 = 4 THEN
-			delete from managers where person_id = $1 returning id into x; 
-		ELSE
-			--DO NOTHING
-		END IF;
-
-		IF $2 = 5 THEN
-			delete from administrators where person_id = $1 returning id into x; 
-		ELSE
-			--DO NOTHING
-		END IF;
-		RAISE LOG 'log message x:%', x;
-
-	END IF;
-
-EXCEPTION
-	WHEN others THEN
-        x := -100;
-	
-END;
-$$;
-
-CREATE OR REPLACE FUNCTION f_update_profile(int,int,int,int)
-RETURNS text AS $$
-DECLARE
-        result_set text;
-        DECLARE x int := -111;
-        json_result text;
-BEGIN
-
-        IF $2 is NULL THEN
-        ELSE
-
-                CALL p_update_profile($2,$3,$4,x);
-
-                IF x > 0 THEN
-                        result_set = CONCAT
-                        (
-                                j_select_persons($1),
-                                ',',
-                                j_select_messages('Profile Updated.'),
-                                ',',
-                                j_select_codes(-101),
-                                ',',
-				j_select_profiles($2)
-                        );
-
-                ELSE
-                        result_set = CONCAT
-                        (
-                                j_select_persons($1),
-                                ',',
-                                j_select_messages('Something went wrong with updating profile. Please try again.'),
-                                ',',
-                                j_select_codes(-101),
-                                ',',
-				j_select_profiles($2)
-                        );
-
-                END IF;
-        END IF;
-
-RETURN result_set;
-END;
-$$ LANGUAGE plpgsql;
-
---END UPDATE PROFILE
-
-
---BEGIN SELECT PROFILES
-CREATE OR REPLACE FUNCTION f_profile(email_id_p int, person_id_p int)
-RETURNS text AS $$
-DECLARE
-        result_set text;
-BEGIN
-        result_set = CONCAT
-	(
-		j_select_persons($1),
-		',',
-		j_select_messages(null),
-		',',
-		j_select_codes(-102),
-		',',
-		j_select_profiles($2)
-	);
-RETURN result_set;
-END;
-$$ LANGUAGE plpgsql;
-
---BEGIN SELECT CLUB PROFILES
-CREATE OR REPLACE FUNCTION f_club_profile(email_id_p int, person_id_p int, club_id_p int)
-RETURNS text AS $$
-DECLARE
-        result_set text;
-	found_club_administrator_id club_administrators.id%TYPE;
-
-BEGIN
-	select club_administrators.id into found_club_administrator_id from club_administrators join club_persons on club_persons.id=club_administrators.club_person_id where club_persons.person_id = $2;
-	IF found_club_administrator_id > 0 THEN
-	        result_set = CONCAT
-        	(
-                	j_select_persons(email_id_p),
-                	',',
-                	j_select_messages(null),
-                	',',
-                	j_select_codes(-102),
-                	',',
-                	j_select_club_profiles(person_id_p, club_id_p)
-        	);
-	ELSE
-	        result_set = CONCAT
-        	(
-                	j_select_persons($1),
-                	',',
-                	j_select_messages('You are not a club administrator.'),
-                	',',
-                	j_select_codes(-101)
-        	);
-
-	END IF;
-RETURN result_set;
-END;
-$$ LANGUAGE plpgsql;
 
 
 --BEGIN SELECT PITCHES
