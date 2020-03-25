46cfe41739f3e8c024e945927033686232b54f97
Jim Breslin <jbreslin33@gmail.com>
2020-03-25 12:27:05 -0400

    websocketd added to install

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
+echo ----------------------install composer------------------------
 sudo apt-get -y install composer 
+echo ----------------------install php mailer------------------------
 composer require phpmailer/phpmailer
+
+echo ----------------------install websocketd------------------------
+sudo apt install ./websocketd-0.3.0_amd64.deb
