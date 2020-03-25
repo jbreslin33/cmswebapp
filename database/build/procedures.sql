07d560e33042e66910c0a6524c9314a26651920b
Jim Breslin <jbreslin33@gmail.com>
2020-03-24 20:15:40 -0400

    choose person

--- database/build/procedures.sql
+++ database/build/procedures.sql
@@ -5,24 +5,7 @@
 
 
 
---BEGIN CHOOSE PERSON
-CREATE OR REPLACE FUNCTION f_choose_person(int)
-RETURNS text AS $$
-DECLARE
-        result_set text;
-BEGIN
-	result_set = CONCAT
-        (
-        	j_select_persons($1),
-       		',',
-               	j_select_messages(null),
-                ',',
-               	j_select_codes(-100)
-       	);
-RETURN result_set;
-END;
-$$ LANGUAGE plpgsql;
---END CHOOSE PERSON
+
 
 
 --BEGIN SELECT EVENTS
