bc58ec7a18cfcc8f60f0835ff67e1fad612b968a
Jim Breslin <jbreslin33@gmail.com>
2020-03-25 22:20:22 -0400

    client con

--- client.cpp
+++ client.cpp
@@ -13,9 +13,8 @@ class Client
 {
 	public:
 
-	Client();		
-  	
 	char buffer[200];
+
 
 	private:
   	
@@ -23,42 +22,48 @@ class Client
   	struct sockaddr_in sa;
   	int bytes_sent;
 
+	public:
 
-};
-
-Client::Client(void)
-
-{
-  	strcpy(this->buffer, "hello world!");
-  
-	sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-  
-	if (sock == -1) 
+	Client()
 	{
-      		printf("Error Creating Socket");
-      		exit(EXIT_FAILURE);
+
 	}
 
-  	memset(&sa, 0, sizeof sa);
-  
-  	sa.sin_family = AF_INET;
- 
-  	sa.sin_addr.s_addr = inet_addr("127.0.0.1");
-  
-  	sa.sin_port = htons(7654);
- 
-  	bytes_sent = sendto(sock, buffer, strlen(buffer), 0,(struct sockaddr*)&sa, sizeof sa);
-  	if (bytes_sent < 0) 
+	void sendToServer()
 	{
-    		printf("Error sending packet: %s\n", strerror(errno));
-    		exit(EXIT_FAILURE);
-  	}
+  		strcpy(this->buffer, "hello world!");
+  
+		sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  
+		if (sock == -1) 
+		{
+      			printf("Error Creating Socket");
+      			exit(EXIT_FAILURE);
+		}
+
+  		memset(&sa, 0, sizeof sa);
+  
+  		sa.sin_family = AF_INET;
  
-  	close(sock); 
-}
+  		sa.sin_addr.s_addr = inet_addr("127.0.0.1");
+  
+  		sa.sin_port = htons(7654);
+ 
+  		bytes_sent = sendto(sock, buffer, strlen(buffer), 0,(struct sockaddr*)&sa, sizeof sa);
+  		if (bytes_sent < 0) 
+		{
+    			printf("Error sending packet: %s\n", strerror(errno));
+    			exit(EXIT_FAILURE);
+	  	}
+ 
+  		close(sock); 
+
+	}
+};
 
 int main(void)
 {
 	Client* client = new Client();
+	client->sendToServer();
   	return 0;
 }
