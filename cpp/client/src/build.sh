bd5529d3582873e945d3bb7f45bd275e877d9d5c
Jim Breslin <jbreslin33@gmail.com>
2020-03-27 17:04:55 -0400

    home

--- cpp/client/src/build.sh
+++ cpp/client/src/build.sh
@@ -1,1 +1,3 @@
 g++ -std=c++11 -pthread -o main main.cpp
+g++ -std=c++11 -pthread -o client client.cpp
+g++ -std=c++11 -pthread -o server server.cpp
--- cpp/client/src/client.cpp
+++ cpp/client/src/client.cpp
@@ -14,25 +14,7 @@
 
 #include "client.h"
 
-class Client
-{
-	private:
-  	
-	int sock;
-  	struct sockaddr_in sa;
-  	int bytes_sent;
-
-	public:
-	
-	char buffer[200];
-
-	Client()
-	{
-
-	}
-
-	//void sendToServer(std::string m)
-	void sendToServer(std::string s)
+	void Client::sendToServer(std::string s)
 	{
 		char cstr[s.size() + 1];
 		strcpy(this->buffer, s.c_str());	// or pass &s[0]
@@ -61,58 +43,6 @@ class Client
 	  	}
  
   		close(sock); 
-
 	}
 
-};
 
-Client* client;
-std::string msg;
-
-void readData()
-{
-	while (true) 
-	{
-		//declare string
-		std::string sin;
-
-		//set any data coming in to var sin
-    		std::cin >> sin;
-
-		//set global msg var to sin
-    		msg = sin;
-  	}
-}
-
-void writeData()
-{
-	while (true) 
-	{
-    		//std::lock_guard<std::mutex> lock{msg_mutex};
-    		if (msg.length() > 0) 
-		{
-			//send to websocketd which will then forward to client web browser
-			//this needs to be done after processing by server....
-      			std::cout << msg << std::endl;
-
-			//send to server for processing
-			client->sendToServer(msg);
-
-			//clear the msg for next time
-      			msg.clear();
-    		}
-  	}
-}
-
-int main(void)
-{
-	client = new Client();
-
-	std::thread reader (readData);     
-	std::thread writer (writeData);     
-	reader.join();
-	writer.join();
-
-	//client->sendToServer();
-  	return 0;
-}
