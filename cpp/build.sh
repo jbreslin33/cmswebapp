4349d114734bb4f9cd1737e50b7fa4cf3d90ebb6
Jim Breslin <jbreslin33@gmail.com>
2020-03-30 20:16:36 -0400

    cpp rm

--- cpp/build.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++11 -pthread -o client client.cpp
--- cpp/client/src/build.sh
+++
@@ -1,7 +0,0 @@
-#g++ -std=c++11 -pthread -o main main.cpp
-#g++ -std=c++11 -pthread -o client client.cpp
-
-#compile client and main
-g++ -std=c++11 -pthread -o client client.cpp main.cpp
-
-
