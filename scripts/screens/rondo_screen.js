895eddc1a3386e3c474e818f4ab92facbd21c00a
Jim Breslin <jbreslin33@gmail.com>
2020-04-02 19:40:44 -0400

    new client connection as soon as you hit rondo button

--- scripts/screens/rondo_screen.js
+++ scripts/screens/rondo_screen.js
@@ -8,7 +8,9 @@ class RondoScreen extends Screen
 
 		location.hash = 'rondo_screen';
 
-		this.mWebSocket = null;
+		//this.mWebSocket = null;
+		//APPLICATION.getCurrentScreen().mWebSocket = new WebSocket('ws://127.0.0.1:8080/');
+
 
           	this.setHtml(document.getElementById("rondo_screen_html_id"));
                 this.setMessageElement(document.getElementById("rondo_screen_message_id"));
@@ -21,36 +23,37 @@ class RondoScreen extends Screen
                         APPLICATION.getCurrentScreen().hit();
                 });
  
-		document.getElementById("rondoscreenjoinbuttonid").onclick = this.hitJoinRondo.bind(document.getElementById("rondoscreenjoinbuttonid"));
+		//document.getElementById("rondoscreenjoinbuttonid").onclick = this.hitJoinRondo.bind(document.getElementById("rondoscreenjoinbuttonid"));
+        
+		//send for new client connection
+		this.mWebSocket = new WebSocket('ws://127.0.0.1:8080/');
+		//this.mWebSocket.send('2');
+
+                this.mWebSocket.onopen = function ()
+                {
+                        //this.send('2'); //2 is connect new client code
+                        APPLICATION.getCurrentScreen().mWebSocket.send('2');
+                }
+
+                this.mWebSocket.onmessage = function(event)
+                {
+                        document.getElementById('rondo_screen_outgoing_message_id').innerHTML = event.data;
+                        document.getElementById('rondo_screen_message_id').value='';
+                }
+
 
 	}
-	/*
-	                                  <div id="rondo_screen_spinner_id" class="spinner-border text-primary" role="status">
-                                                <span class="sr-only">Loading...</span>
-                                        </div>
-
-                                        <label for="rondo_name_id">Message to other Players:</label>
-                                        <input type="text" id="rondo_screen_outgoing_message_id">
-                                        <button id="rondoscreensendbuttonid">Send Message</button>
-
-                                        <span id='rondo_screen_message_id'></span>
-
-                                        <button id="rondoscreenjoinbuttonid">Join Rondo</button>
-*/
-
+	
 	hit()
 	{
-      		//var name  = document.getElementById("rondo_screen_name_id").value;
-               	//var address = document.getElementById("rondo_screen_address_id").value;
-	
 		APPLICATION.getCurrentScreen().setUrl("/php/classes/screens/rondo.php?" + this.getStandardParameters()); 
 		APPLICATION.getCurrentScreen().ajax();
 	}
 
-	hitJoinRondo()
-	{
-		APPLICATION.getCurrentScreen().mWebSocket = new WebSocket('ws://127.0.0.1:8080/');
-
+	//hitJoinRondo()
+	//{
+	//	APPLICATION.getCurrentScreen().mWebSocket = new WebSocket('ws://127.0.0.1:8080/');
+/*
         	APPLICATION.getCurrentScreen().mWebSocket.onopen = function ()
         	{
         		//this.send('2'); //2 is connect new client code
@@ -62,7 +65,8 @@ class RondoScreen extends Screen
         		document.getElementById('rondo_screen_outgoing_message_id').innerHTML = event.data;
                		document.getElementById('rondo_screen_message_id').value='';
         	}
-	}
+		*/
+//	}
 /*
         send()
        	{ 
