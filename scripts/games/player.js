4179c6e04e104690f8e44b40403ee80956c2eb21
Jim Breslin <jbreslin33@gmail.com>
2020-04-26 18:32:30 -0400

    coords

--- scripts/games/player.js
+++ scripts/games/player.js
@@ -12,62 +12,22 @@ class Player
 		this.mLooking = 0.0;
 		this.mFacingAngle = 0.0;
 
-		this.drawX = x;
-		this.drawY = y;
-
 		this.mColor = color;
 
 		this.mId = id;
 
 		this.mDiameter = 3.0;
-		this.mSize = 0.0; //at first
+		this.mSize = 3.0; //at first
 
 		this.mAngle = 1;
 	}
 
 	update()
 	{
-		//update player size based on physical screen size	
-		var physicalWidth = this.mPitch.mScreen.mCanvas.width; 
-		var physicalHeight = this.mPitch.mScreen.mCanvas.height; 
-		var pixelsPerMeterOfFieldWidth = physicalWidth / 800;
-		var pixelsPerMeterOfFieldHeigth = physicalHeight / 600;
-		//this.mSize = pixelsPerMeterOfFieldWidth * .41; 
-		this.mSize = pixelsPerMeterOfFieldWidth * this.mDiameter; 
-
-		//translate server and real world cartesian coordinate of player to this screen
-		var originX = physicalWidth / 2; 
-		var originY = physicalHeight / 2; 
-
-		var fieldWidth = 800;
-		var fieldHeigt = 600; 
-		var eastTouchLine = -52.5;
-		var westTouchLine = 52.5;
-		var northTouchLine = 34
-		var southTouchLine = -34
-		//so if you are at 5x then you are 5 * pixelsPerMeterOfFieldWidth
-
-		this.drawX = (this.x * pixelsPerMeterOfFieldWidth) + originX; 
-		this.drawY = (this.y * pixelsPerMeterOfFieldWidth) + originY; 
-
-		var f = this.mFacingAngle;
-		var newFacingAngle;
-		if (f < 0)
-		{
-			newFacingAngle = 360 - (f * -1);
-			this.mFacingAngle = newFacingAngle;
-		}
-			
-		this.mAngle = this.mFacingAngle * Math.PI / 180;
-
-
 
 		this.mContext.save();
-		this.mContext.translate(this.drawX,this.drawY);
-		this.mContext.rotate(this.mAngle);
 
 		this.drawCircleMan();	
-		//this.drawLine();
 
 		this.mContext.restore();
 		this.mContext.setTransform(1, 0, 0, 1, 0, 0);
@@ -76,12 +36,9 @@ class Player
 	drawCircleMan()
 	{
       		this.mContext.beginPath();
-      		this.mContext.arc(0, 0, this.mSize / 2, 0, 2 * Math.PI, false);
+      		this.mContext.arc(this.x, this.y, this.mSize / 2, 0, 2 * Math.PI, false);
       		this.mContext.fillStyle = this.mColor;
       		this.mContext.fill();
-      		//this.mContext.lineWidth = 5;
-      		//this.mContext.strokeStyle = '#003300';
-      		//this.mContext.stroke();
 	}
 
 
