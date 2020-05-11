160709900199dc65f2fd5531e5e748985fa51d3d
Jim Breslin <jbreslin33@gmail.com>
2020-05-11 10:44:34 -0400

    roles show up proper buttons, but do not update on change person

--- scripts/screens/side_screen.js
+++ scripts/screens/side_screen.js
@@ -24,24 +24,61 @@ class SideScreen extends Screen
 
 	handleButtons()
 	{
-		console.log('called handle buttons array length: ' + this.mApplication.mPersonArray.length);
+		//set all buttons to none except schedule, logout, persons, edit profile so we can bring them up if role needs them
+          	document.getElementById("side_games_id").style.display = "none";
+
+		document.getElementById("side_schedule_id").style.display = "block";
+               	document.getElementById("side_schedule_id").style.visibility = "visible";
+		
+		document.getElementById("side_logout_id").style.display = "block";
+               	document.getElementById("side_logout_id").style.visibility = "visible";
+		
+		document.getElementById("side_managers_id").style.display = "none";
+          	
+		document.getElementById("side_club_administrator_id").style.display = "none";
+		
+		document.getElementById("side_persons_id").style.display = "block";
+               	document.getElementById("side_persons_id").style.visibility = "visible";
+
+          	document.getElementById("side_administration_id").style.display = "none";
+
+		document.getElementById("side_profile_id").style.display = "block";
+               	document.getElementById("side_profile_id").style.visibility = "visible";
+
 		//loop throu array
 		for (var i = 0; i < this.mApplication.mPersonArray.length; i++)
 		{
 			if (this.mApplication.mPersonArray[i].mId == this.getPersonId())
 			{
-				console.log('calling handle but on personID: ' + this.getPersonId());
-				console.log('calling handle but on managerId: ' + this.mApplication.mPersonArray[i].mManagerId  );
-				if (this.mApplication.mPersonArray[i].mManagerId == null) 
+				var player = this.mApplication.mPersonArray[i].mPlayerId;
+				var family = this.mApplication.mPersonArray[i].mFamilyId;
+				var coach = this.mApplication.mPersonArray[i].mCoachId;
+				var manager = this.mApplication.mPersonArray[i].mManagerId;
+				var administrator = this.mApplication.mPersonArray[i].mAdministratorId;
+
+				if (player != null) 
 				{
-          				document.getElementById("side_managers_id").style.display = "none";
-					console.log('hide but');
+                			document.getElementById("side_games_id").style.display = "block";
+                			document.getElementById("side_games_id").style.visibility = "visible";
 				}
-				else
+				if (family != null) 
+				{
+				}
+				if (coach != null) 
+				{
+				}
+				if (manager != null) 
 				{
                 			document.getElementById("side_managers_id").style.display = "block";
                 			document.getElementById("side_managers_id").style.visibility = "visible";
-					console.log('show but');
+				}
+				if (administrator != null) 
+				{
+                			document.getElementById("side_club_administrator_id").style.display = "block";
+                			document.getElementById("side_club_administrator_id").style.visibility = "visible";
+                			
+					document.getElementById("side_administration_id").style.display = "block";
+                			document.getElementById("side_administration_id").style.visibility = "visible";
 				}
 			}
 		}
