8ec4c17bf3fbbdb48a086fbca4aa5001b1dcb2d6
Jim Breslin <jbreslin33@gmail.com>
2020-05-16 19:39:56 -0400

    do we have it?

--- database/query.sql
+++ database/query.sql
@@ -34,17 +34,25 @@
 			;
 
 		select 
-			* 
+			teams.name, 
+			team_club_persons_club_players.id 
+			
 			from 
 				team_club_persons_club_players
-
-		join 
+		full outer join 
 			team_club_persons on team_club_persons.id=team_club_persons_club_players.team_club_person_id			
-		join 
+		full outer join 
 			teams on teams.id=team_club_persons.team_id			
 
+		join    
+			club_persons on club_persons.id=team_club_persons.club_person_id
+		join    
+			persons on persons.id=club_persons.person_id
+
 		where 
-			teams.club_id = 1
+			--teams.club_id = 1
+			persons.id = 25 AND teams.club_id = 1
 		;
 		--where persons.id = 25 AND teams.club_id = 1
+
 
