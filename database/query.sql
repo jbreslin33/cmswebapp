f7830a0fac08007ecda0c6cdec3bdd78d15ba449
Jim Breslin <jbreslin33@gmail.com>
2020-05-16 18:16:02 -0400

    do we have it for players???

--- database/query.sql
+++ database/query.sql
@@ -1,25 +1,3 @@
-                select
-                persons.last_name as last_name,
-                persons.first_name as first_name,
-                persons.dob as dob,
-                persons.id as person_id,
-                players.id as player_id,
-                parents.id as parent_id,
-                coaches.id as coach_id,
-                managers.id as manager_id,
-                administrators.id as administrator_id
-
-                from persons
-
-                left join players on players.person_id=persons.id
-                left join parents on parents.person_id=persons.id
-                left join coaches on coaches.person_id=persons.id
-                left join managers on managers.person_id=persons.id
-                left join administrators on administrators.person_id=persons.id
-		where persons.id = 25 
-                order by persons.last_name asc;
-
-
 		select 
 			teams.id as team_id, 
 			teams.name as team_name, 
@@ -45,6 +23,28 @@
 		left join 
 			team_club_persons_club_managers on team_club_persons_club_managers.team_club_person_id=team_club_persons.id
 
-		
-		where persons.id = 25 AND teams.club_id = 1;
+		where persons.id = 25 AND teams.club_id = 1
+		group by 
+			team_name, 
+			teams.id, 
+			team_club_persons_club_players.id,
+			team_club_persons_club_parents.id,
+			team_club_persons_club_coaches.id,
+			team_club_persons_club_managers.id
+			;
+
+		select 
+			* 
+			from 
+				team_club_persons_club_players
+
+		join 
+			team_club_persons on team_club_persons.id=team_club_persons_club_players.team_club_person_id			
+		join 
+			teams on teams.id=team_club_persons.team_id			
+
+		where 
+			teams.club_id = 1
+		;
+		--where persons.id = 25 AND teams.club_id = 1
 
