e3d14bf0dfdc739688b37a3ebf55e7ef388084ad
Jim Breslin <jbreslin33@gmail.com>
2020-05-16 16:20:57 -0400

    dups

--- database/query.sql
+++ database/query.sql
@@ -16,5 +16,35 @@
                 left join coaches on coaches.person_id=persons.id
                 left join managers on managers.person_id=persons.id
                 left join administrators on administrators.person_id=persons.id
+		where persons.id = 25 
                 order by persons.last_name asc;
+
+
+		select 
+			teams.id as team_id, 
+			teams.name as team_name, 
+			team_club_persons_club_players.id as team_club_persons_club_player_id,
+			team_club_persons_club_parents.id as team_club_persons_club_parent_id,
+			team_club_persons_club_coaches.id as team_club_persons_club_coach_id,
+			team_club_persons_club_managers.id as team_club_persons_club_manager_id
+		from 
+			teams
+
+		full outer join 
+			club_persons on club_persons.club_id=teams.club_id
+		full outer join 
+			persons on persons.id=club_persons.person_id
+		left join 
+			team_club_persons on team_club_persons.club_person_id=club_persons.id
+		left join 
+			team_club_persons_club_players on team_club_persons_club_players.team_club_person_id=team_club_persons.id
+		left join 
+			team_club_persons_club_parents on team_club_persons_club_parents.team_club_person_id=team_club_persons.id
+		left join 
+			team_club_persons_club_coaches on team_club_persons_club_coaches.team_club_person_id=team_club_persons.id
+		left join 
+			team_club_persons_club_managers on team_club_persons_club_managers.team_club_person_id=team_club_persons.id
+
+		
+		where persons.id = 25 AND teams.club_id = 1;
 
