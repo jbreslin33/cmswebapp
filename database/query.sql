e37376fe0af0ec41fd3766483c8c7f45a4fc8db7
Jim Breslin <jbreslin33@gmail.com>
2020-05-16 19:53:12 -0400

    getting no players for jim which is right and 3 manager spots on all 3 teams which is right

--- database/query.sql
+++ database/query.sql
@@ -1,39 +1,3 @@
-		select 
-			teams.id as team_id, 
-			teams.name as team_name, 
-			team_club_persons_club_players.id as team_club_persons_club_player_id,
-			team_club_persons_club_parents.id as team_club_persons_club_parent_id,
-			team_club_persons_club_coaches.id as team_club_persons_club_coach_id,
-			team_club_persons_club_managers.id as team_club_persons_club_manager_id
-		from 
-			teams
-
-		full outer join 
-			club_persons on club_persons.club_id=teams.club_id
-		full outer join 
-			persons on persons.id=club_persons.person_id
-		left join 
-			team_club_persons on team_club_persons.club_person_id=club_persons.id
-		left join 
-			team_club_persons_club_players on team_club_persons_club_players.team_club_person_id=team_club_persons.id
-		left join 
-			team_club_persons_club_parents on team_club_persons_club_parents.team_club_person_id=team_club_persons.id
-		left join 
-			team_club_persons_club_coaches on team_club_persons_club_coaches.team_club_person_id=team_club_persons.id
-		left join 
-			team_club_persons_club_managers on team_club_persons_club_managers.team_club_person_id=team_club_persons.id
-
-		where persons.id = 25 AND teams.club_id = 1
-		group by 
-			team_name, 
-			teams.id, 
-			team_club_persons_club_players.id,
-			team_club_persons_club_parents.id,
-			team_club_persons_club_coaches.id,
-			team_club_persons_club_managers.id
-			;
-
-
 
 		select 
 			teams.name, 
@@ -66,9 +30,5 @@
 			persons on persons.id=club_persons.person_id
 
 		where 
-			--teams.club_id = 1
 			persons.id = 25 AND teams.club_id = 1
 		;
-		--where persons.id = 25 AND teams.club_id = 1
-
-
