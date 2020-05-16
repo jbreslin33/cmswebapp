0f5042d414a4e6fdb485f3a40114a0314159ebd7
Jim Breslin <jbreslin33@gmail.com>
2020-05-16 19:49:43 -0400

    getting no players for jim which is right and 3 manager spots on all 3 teams which is right

--- database/query.sql
+++ database/query.sql
@@ -33,14 +33,21 @@
 			team_club_persons_club_managers.id
 			;
 
+
+
 		select 
 			teams.name, 
-			team_club_persons_club_players.id as player
+			team_club_persons_club_players.id as player,
+			team_club_persons_club_managers.id as manager
 			
 			from 
-				team_club_persons_club_players
+				team_club_persons 
+
 		full outer join 
-			team_club_persons on team_club_persons.id=team_club_persons_club_players.team_club_person_id			
+			team_club_persons_club_players on team_club_persons_club_players.team_club_person_id=team_club_persons.id		
+		full outer join 
+			team_club_persons_club_managers on team_club_persons_club_managers.team_club_person_id=team_club_persons.id		
+
 		full outer join 
 			teams on teams.id=team_club_persons.team_id			
 
