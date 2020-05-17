aa4842b4bed8ab312d4e95e6e2765b16c39f0e21
Jim Breslin <jbreslin33@gmail.com>
2020-05-17 10:15:39 -0400

    getting teams, club_players, team_players

--- database/query.sql
+++ database/query.sql
@@ -22,50 +22,23 @@
 			persons.id = 24 AND club_persons.club_id = 1
 
 		;
-		
 
 
 		select 
-                        teams.id as team_id,
-                        teams.name as team_name,
-
-                        team_club_persons.id as team_club_person_id,
-
-                        club_players.id as club_players_id,
-                        club_parents.id as club_parents_id,
-                        club_coaches.id as club_coaches_id,
-                        club_managers.id as club_managers_id,
-
-                        team_club_persons_club_players.id as player,
-                        team_club_persons_club_parents.id as parent,
-                        team_club_persons_club_coaches.id as coach,
-                        team_club_persons_club_managers.id as manager
+                        team_club_persons.team_id as team_id,
+                        team_club_persons_club_players.id as player
 
 			from 
-				teams 
-
-		join team_club_persons on team_club_persons.team_id=teams.id
+				team_club_persons_club_players
+		join
+			team_club_persons on team_club_persons.id=team_club_persons_club_players.team_club_person_id
 
 		join    
 			club_persons on club_persons.id=team_club_persons.club_person_id
-		
-		join    
-			club_players on club_players.club_person_id=club_persons.id
-		
-		join    
-			club_parents on club_parents.club_person_id=club_persons.id
-		
-		join    
-			club_coaches on club_coaches.club_person_id=club_persons.id
-		
-		join    
-			club_managers on club_managers.club_person_id=club_persons.id
 
-		join    
-			persons on persons.id=club_persons.person_id
 
 		where 
-			persons.id = 24 AND teams.club_id = 1
-		;
+			club_persons.person_id = 24 AND club_persons.club_id = 1
 
-		select * from team_club_persons_club_managers;
+		;		
+
