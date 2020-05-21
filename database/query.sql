ec2b7476269ece6b06645d60ae74228ac7f463bf
Jim Breslin <jbreslin33@gmail.com>
2020-05-21 17:11:42 -0400

    query now will grab multiple entrys, up to javascript on other end to sort or db end. also what to wrtie would be an entry for player: Luke Parent: Jim Breslin etc....

--- database/query.sql
+++ database/query.sql
@@ -1,4 +1,4 @@
-                select practices.id, practices.event_date, practices.arrival_time, practices.start_time, practices.end_time, practices.address, practices.coordinates,(select pitches.name from pitches where practices.pitch_id = pitches.id) as pitch_name, practices.field_name, clubs.name as club_name, teams.name as team_name, team_club_persons_club_players.id as team_club_persons_club_players_id, persons.first_name, persons.last_name, (select practices_players_availability.availability_id from practices_players_availability where practices_players_availability.practice_id = practices.id) as availability_id
+                select practices.id, practices.event_date, practices.arrival_time, practices.start_time, practices.end_time, practices.address, practices.coordinates,(select pitches.name from pitches where practices.pitch_id = pitches.id) as pitch_name, practices.field_name, clubs.name as club_name, teams.name as team_name, persons.first_name, persons.last_name, (select practices_players_availability.availability_id from practices_players_availability where practices_players_availability.practice_id = practices.id) as availability_id, team_club_persons_club_players.id as players, team_club_persons_club_parents.id as parents, team_club_persons_club_coaches.id as coaches, team_club_persons_club_managers.id as managers
                 from practices
                 join practice on practice.id=practices.practice_id
                 join teams on teams.id=practice.team_id
@@ -14,7 +14,9 @@
 
                 --join team_club_persons_club_players on team_club_persons_club_players.team_club_person_id=team_club_persons.id AND team_club_persons_club_players.club_player_id=club_players.id
                 left outer join team_club_persons_club_players on team_club_persons_club_players.team_club_person_id=team_club_persons.id --AND team_club_persons_club_players.club_player_id=club_players.id
-                --left outer join team_club_persons_club_parents on team_club_persons_club_parents.team_club_person_id=team_club_persons.id --AND team_club_persons_club_players.club_player_id=club_players.id
+                left outer join team_club_persons_club_parents on team_club_persons_club_parents.team_club_person_id=team_club_persons.id --AND team_club_persons_club_players.club_player_id=club_players.id
+                left outer join team_club_persons_club_coaches on team_club_persons_club_coaches.team_club_person_id=team_club_persons.id --AND team_club_persons_club_players.club_player_id=club_players.id
+                left outer join team_club_persons_club_managers on team_club_persons_club_managers.team_club_person_id=team_club_persons.id --AND team_club_persons_club_players.club_player_id=club_players.id
 
                 where emails_persons.email_id = 21 
 
