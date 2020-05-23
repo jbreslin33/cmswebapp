df1ced1167be569a9fbfcb3f3e6f29120279b497
Jim Breslin <jbreslin33@gmail.com>
2020-05-23 19:17:18 -0400

    schedule handles multiple players on one team

--- database/build/selects.sql
+++ database/build/selects.sql
@@ -653,7 +653,13 @@ BEGIN
 SELECT json_agg(t) INTO raw_json
         from
         (
-                 select practices.id, practices.event_date, practices.arrival_time, practices.start_time, practices.end_time, practices.address, practices.coordinates,(select pitches.name from pitches where practices.pitch_id = pitches.id) as pitch_name, practices.field_name, clubs.name as club_name, teams.name as team_name, persons.first_name, persons.last_name, (select practices_players_availability.availability_id from practices_players_availability where practices_players_availability.practice_id = practices.id) as availability_id, team_club_persons_club_players.id as players, team_club_persons_club_parents.id as parents, team_club_persons_club_coaches.id as coaches, team_club_persons_club_managers.id as managers
+                 select practices.id, practices.event_date, practices.arrival_time, practices.start_time, practices.end_time, practices.address, practices.coordinates,(select pitches.name from pitches where practices.pitch_id = pitches.id) as pitch_name, practices.field_name, clubs.name as club_name, teams.name as team_name, persons.first_name, persons.last_name, 
+
+ 
+		practices_players_availability.availability_id as availability_id,
+
+
+		team_club_persons_club_players.id as players, team_club_persons_club_parents.id as parents, team_club_persons_club_coaches.id as coaches, team_club_persons_club_managers.id as managers
                 from practices
                 join practice on practice.id=practices.practice_id
                 join teams on teams.id=practice.team_id
@@ -669,8 +675,7 @@ SELECT json_agg(t) INTO raw_json
                 left outer join team_club_persons_club_parents on team_club_persons_club_parents.team_club_person_id=team_club_persons.id
                 left outer join team_club_persons_club_coaches on team_club_persons_club_coaches.team_club_person_id=team_club_persons.id
                 left outer join team_club_persons_club_managers on team_club_persons_club_managers.team_club_person_id=team_club_persons.id
-
-		--join team_club_persons_club_players on team_club_persons_club_players.team_club_person_id=team_club_persons.id AND team_club_persons_club_players.club_player_id=club_players.id
+		left outer join practices_players_availability on practices_players_availability.team_club_persons_club_players_id = team_club_persons_club_players.id
 
                 where emails_persons.email_id = $1 AND practices.event_date > $2 - interval '1 day' AND practices.event_date < $3
 	) t;
@@ -713,6 +718,7 @@ SELECT json_agg(t) INTO raw_json
                 left outer join team_club_persons_club_parents on team_club_persons_club_parents.team_club_person_id=team_club_persons.id
                 left outer join team_club_persons_club_coaches on team_club_persons_club_coaches.team_club_person_id=team_club_persons.id
                 left outer join team_club_persons_club_managers on team_club_persons_club_managers.team_club_person_id=team_club_persons.id
+		left outer join games_players_availability on games_players_availability.team_club_persons_club_players_id = team_club_persons_club_players.id
 
 
                 where emails_persons.email_id = $1 AND games.event_date > $2 - interval '1 day' AND games.event_date < $3
