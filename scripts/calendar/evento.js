3a66297446403f590a5b44682a3d1b660ae863a5
Jim Breslin <jbreslin33@gmail.com>
2020-05-24 14:13:00 -0400

    button color

--- scripts/calendar/evento.js
+++ scripts/calendar/evento.js
@@ -87,10 +87,11 @@ class Evento
                                 	button.style.backgroundColor = "red";
                                 }
                                 this.mButtonArray.push(button);
-
+/*
 				var p = document.createElement('p');
                        		this.mContainerDiv.appendChild(p);
 				p.innerHTML = '<br>';
+				*/
 			}
 		}
 
