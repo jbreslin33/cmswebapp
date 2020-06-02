817d121b82c380370a51ee6bbc8d7d2ce5f5af3f
Jim Breslin <jbreslin33@gmail.com>
2020-06-02 19:32:28 -0400

    insert works perfect. delete does not

--- scripts/screens/insert_person_screen.js
+++ scripts/screens/insert_person_screen.js
@@ -43,6 +43,12 @@ class InsertPersonScreen extends Screen
                 var lastName  = document.getElementById("insert_person_screen_last_name_id").value;
                 var phone = document.getElementById("insert_person_screen_phone_id").value;
                 var address = document.getElementById("insert_person_screen_address_id").value;
+                
+		document.getElementById("insert_person_screen_first_name_id").value = null;
+                document.getElementById("insert_person_screen_middle_name_id").value = null;
+                document.getElementById("insert_person_screen_last_name_id").value = null;
+                document.getElementById("insert_person_screen_phone_id").value = null;
+                document.getElementById("insert_person_screen_address_id").value = null;
 
                 APPLICATION.getCurrentScreen().setUrl("/php/classes/screens/insert_person.php?" + this.getStandardParameters() + "&first_name=" + firstName + "&middle_name=" + middleName + "&last_name=" + lastName + "&phone=" + phone + "&address=" + address);
                 APPLICATION.getCurrentScreen().ajax();
