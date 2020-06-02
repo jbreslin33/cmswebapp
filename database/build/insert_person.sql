c2b49d50fc32e48a17f3b96045456bef59712b88
Jim Breslin <jbreslin33@gmail.com>
2020-06-02 19:15:12 -0400

    stay on screen works. next rid of text box text

--- database/build/insert_person.sql
+++ database/build/insert_person.sql
@@ -17,7 +17,7 @@ BEGIN
                 	',',
                 	j_select_messages(null),
                 	',',
-                	j_select_codes(-100)
+                	j_select_codes(-101)
         	);
 
         ELSE
