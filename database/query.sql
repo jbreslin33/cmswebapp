310eac313f2b84d613891eb5dedc32a1a66fcf61
Jim Breslin <jbreslin33@gmail.com>
2020-06-14 10:03:10 -0400

    query getting more info

--- database/query.sql
+++ database/query.sql
@@ -11,5 +11,8 @@ from
 	team_club_persons_club_players
 left outer join games_players_availability on games_players_availability.team_club_persons_club_players_id = team_club_persons_club_players.id 
 
+	join team_club_persons on team_club_persons.id = team_club_persons_club_players.team_club_person_id
+
 		;	
 
+	
