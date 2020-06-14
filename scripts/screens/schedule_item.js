9e0a6441ee4ac87716ea0690337c94047087baec
Jim Breslin <jbreslin33@gmail.com>
2020-06-13 21:43:23 -0400

    hide and show'

--- scripts/screens/schedule_item.js
+++ scripts/screens/schedule_item.js
@@ -21,62 +21,99 @@ class ScheduleItem extends Item
 
 		//availability buttons unique to schedules
 		this.mAvailabilityButtonArray = new Array();
+
+		this.mTabRow = null;
+		this.mPlayerDiv = null;
+        	this.mTeamDiv = null;
+	}
+/*
+        showForm()
+        {
+                if (this.getForm())
+                {
+                        this.getForm().style.display = "block";
+                        this.getForm().style.visibility = "visible";
+                }
+        }
+
+        hideForm()
+        {
+                if (this.getForm())
+                {
+                        this.getForm().style.display = "none";
+                }
+        }
+	*/
+
+	hitPlayerDiv()
+	{
+		this.showPlayerDiv();
+		this.hideTeamDiv();
 	}
 
-	showPlayerAvailability()
+	hitTeamDiv()
 	{
-		console.log('showPlayerAvailability()');
+		this.showTeamDiv();
+		this.hidePlayerDiv();
 	}
 
-	showTeamAvailability()
+	showPlayerDiv()
 	{
-		console.log('showTeamAvailability()');
+                if (this.mPlayerDiv)
+                {
+                        this.mPlayerDiv.style.display = "block";
+                        this.mPlayerDiv.style.visibility = "visible";
+                }
 	}
+
+	hidePlayerDiv()
+	{
+                if (this.mPlayerDiv)
+                {
+                        this.mPlayerDiv.style.display = "none";
+                }
+	}
+
+        showTeamDiv()
+        {
+                if (this.mTeamDiv)
+                {
+                        this.mTeamDiv.style.display = "block";
+                        this.mTeamDiv.style.visibility = "visible";
+                }
+        }
+
+        hideTeamDiv()
+        {
+                if (this.mTeamDiv)
+                {
+                        this.mTeamDiv.style.display = "none";
+                }
+        }
 
 	makeTabs()
 	{
 
-        	var row = document.createElement("DIV");
-                row.setAttribute("class","row");
-		this.mContainerDiv.appendChild(row);
+        	this.mTabRow = document.createElement("DIV");
+                this.mTabRow.setAttribute("class","row");
+		this.mContainerDiv.appendChild(this.mTabRow);
 
         	var tab = document.createElement("DIV");
                 tab.setAttribute("class","tab");
-                row.appendChild(tab);
+                this.mTabRow.appendChild(tab);
                 
         	
 		var button = document.createElement("BUTTON");
                 button.setAttribute("class","tab-button");
                 button.innerHTML = this.mTitleText;
-                button.onclick = this.showPlayerAvailability.bind(button);
+                button.onclick = this.hitPlayerDiv.bind(this);
                 tab.appendChild(button);
 		
 		var button = document.createElement("BUTTON");
                 button.setAttribute("class","tab-button");
                 button.innerHTML = 'Team Availability';
-                button.onclick = this.showTeamAvailability.bind(button);
+                button.onclick = this.hitTeamDiv.bind(this);
                 tab.appendChild(button);
-        /*	
-		var br = document.createElement("br");
-                this.mContainerDiv.appendChild(br);
-		*/
-			
-
-/*
-        	var button = document.createElement("BUTTON");
-                                button.setAttribute("class","availability-button");
-                                button.innerHTML = '' + this.mPlayerNameArray[i];
-                                this.mContainerDiv.appendChild(button);
-                                var id = 'button_1_1_' + this.mJson.id + '_' + this.mPlayerIdArray[i];
-                                button.setAttribute("id", id);
-                                button.onclick = this.mApplication.getCurrentScreen().setOneHit.bind(button);
-                                if (availability_id == 1)
-                                {
-                                        button.style.backgroundColor = APPLICATION.mLawnGreen;
-                                }
-                                this.mAvailabilityButtonArray.push(button);
-				*/
-
 	}
 
 	makeButtons()
@@ -98,7 +135,7 @@ class ScheduleItem extends Item
                                 var button = document.createElement("BUTTON");
                                 button.setAttribute("class","availability-button");
                                 button.innerHTML = '' + this.mPlayerNameArray[i];
-                                this.mContainerDiv.appendChild(button);
+                                this.mPlayerDiv.appendChild(button);
                                 var id = 'button_1_1_' + this.mJson.id + '_' + this.mPlayerIdArray[i];
                                 button.setAttribute("id", id);
                                 button.onclick = this.mApplication.getCurrentScreen().setOneHit.bind(button);
@@ -112,7 +149,7 @@ class ScheduleItem extends Item
                                 var button = document.createElement("BUTTON");
                                 button.setAttribute("class","availability-button");
                                 button.innerHTML = '' + this.mPlayerNameArray[i];
-                                this.mContainerDiv.appendChild(button);
+                                this.mPlayerDiv.appendChild(button);
                                 var id = 'button_1_2_' + this.mJson.id + '_' + this.mPlayerIdArray[i];
                                 button.setAttribute("id", id);
                                 button.onclick = this.mApplication.getCurrentScreen().setOneHit.bind(button);
@@ -125,7 +162,7 @@ class ScheduleItem extends Item
                                 var button = document.createElement("BUTTON");
                                 button.setAttribute("class","availability-button");
                                 button.innerHTML = '' + this.mPlayerNameArray[i];
-                                this.mContainerDiv.appendChild(button);
+                                this.mPlayerDiv.appendChild(button);
                                 var id = 'button_1_3_' + this.mJson.id + '_' + this.mPlayerIdArray[i];
                                 button.setAttribute("id", id);
                                 button.onclick = this.mApplication.getCurrentScreen().setOneHit.bind(button);
@@ -149,7 +186,7 @@ class ScheduleItem extends Item
         			var button = document.createElement("BUTTON");
                 		button.setAttribute("class","availability-button");
                         	button.innerHTML = '' + this.mPlayerNameArray[i];
-                        	this.mContainerDiv.appendChild(button);
+                        	this.mPlayerDiv.appendChild(button);
                         	var id = 'button_2_1_' + this.mJson.id + '_' + this.mPlayerIdArray[i];
                         	button.setAttribute("id", id);
                         	button.onclick = this.mApplication.getCurrentScreen().setOneHit.bind(button);
@@ -163,7 +200,7 @@ class ScheduleItem extends Item
                                 var button = document.createElement("BUTTON");
                                 button.setAttribute("class","availability-button");
                         	button.innerHTML = '' + this.mPlayerNameArray[i];
-                                this.mContainerDiv.appendChild(button);
+                                this.mPlayerDiv.appendChild(button);
                         	var id = 'button_2_2_' + this.mJson.id + '_' + this.mPlayerIdArray[i];
                                 button.setAttribute("id", id);
                         	button.onclick = this.mApplication.getCurrentScreen().setOneHit.bind(button);
@@ -176,7 +213,7 @@ class ScheduleItem extends Item
                                 var button = document.createElement("BUTTON");
                                 button.setAttribute("class","availability-button");
                         	button.innerHTML = '' + this.mPlayerNameArray[i];
-                                this.mContainerDiv.appendChild(button);
+                                this.mPlayerDiv.appendChild(button);
                         	var id = 'button_2_3_' + this.mJson.id + '_' + this.mPlayerIdArray[i];
                                 button.setAttribute("id", id);
                         	button.onclick = this.mApplication.getCurrentScreen().setOneHit.bind(button);
@@ -204,23 +241,18 @@ class ScheduleItem extends Item
                 	this.mCardDiv.appendChild(this.mContainerDiv);
                 	this.mDivArray.push(this.mContainerDiv);
 
-/*
-			if (this.mTitleText)
-                	{
-                		this.mTitle = document.createElement('h5');
-                        	this.mContainerDiv.appendChild(this.mTitle);
-			}
-			*/
-			
 			this.makeTabs();
-			
-			//this.mTitle.innerHTML = this.mTitleText;
+
+			//player div..
+			this.mPlayerDiv = document.createElement('div');
+			this.mContainerDiv.appendChild(this.mPlayerDiv);
+
 
                        	var p = document.createElement('p');
 
 			this.makeButtons();
 
-                       	this.mContainerDiv.appendChild(p);
+                       	this.mPlayerDiv.appendChild(p);
 
                         for (var r = 0; r < this.mTextArray.length; r++)
                         {
@@ -243,7 +275,7 @@ class ScheduleItem extends Item
 			       	var button = document.createElement("BUTTON");
                                 button.setAttribute("class","delete-button");
                                 button.innerHTML = 'DELETE ' + this.mJson.type;
-                                this.mContainerDiv.appendChild(button);
+                                this.mPlayerDiv.appendChild(button);
 
 				var id = this.mJson.id;
                                 button.setAttribute("id", id);
