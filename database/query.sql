ad456ce64b0864c5db3b127f6168b82df0821e56
Jim Breslin <jbreslin33@gmail.com>
2020-06-14 09:51:31 -0400

    query getting more info

--- database/query.sql
+++ database/query.sql
@@ -27,6 +27,24 @@
 	where
 		game_id = 1;	
 
-	
+                select games_players_availability.id, games_players_availability.game_id, games_players_availability.team_club_persons_club_players_id, games_players_availability.availability_id
 
+                from
+                        games_players_availability
+
+                        left outer join team_club_persons_club_players on team_club_persons_club_players.id=games_players_availability.team_club_persons_club_players_id
+                        left outer join team_club_persons on team_club_persons.id=team_club_persons_club_players.team_club_person_id
+                        left outer join club_persons on club_persons.id=team_club_persons.club_person_id
+                        left outer join emails_persons on emails_persons.person_id = club_persons.person_id
+
+                        where games_players_availability.game_id = 1 
+			;
+	
+	select 
+		* 
+	from 
+		team_club_persons_club_players
+	left outer join games_players_availability on games_players_availability.team_club_persons_club_players_id = team_club_persons_club_players.id 
+
+		;	
 
