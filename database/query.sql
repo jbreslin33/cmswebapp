abfb9dc455295ca9d558ca0575752af9e54e7344
Jim Breslin <jbreslin33@gmail.com>
2020-06-14 10:05:57 -0400

    compare with roster

--- database/query.sql
+++ database/query.sql
@@ -6,12 +6,16 @@ from
 ;
 
 select 
-	* 
+	persons.first_name, persons.last_name, games_players_availability.availability_id 
 from 
 	team_club_persons_club_players
 left outer join games_players_availability on games_players_availability.team_club_persons_club_players_id = team_club_persons_club_players.id 
 
 	join team_club_persons on team_club_persons.id = team_club_persons_club_players.team_club_person_id
+	join club_persons on club_persons.id = team_club_persons.club_person_id
+	join persons on persons.id = club_persons.person_id
+	
+
 
 		;	
 
