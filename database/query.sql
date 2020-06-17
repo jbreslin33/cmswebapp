2ec2efd110521a567169ec1efb0687fe550fc312
Jim Breslin <jbreslin33@gmail.com>
2020-06-16 21:44:51 -0400

    players on team

--- database/query.sql
+++ database/query.sql
@@ -24,12 +24,16 @@
 
 		select 
 			--team_club_players.id as team_club_player_id 
-			games.id as game_id, team_club_players.id as players 
+			games.id as game_id, team_club_players.id as players, persons.first_name, persons.last_name 
 		from 
 			games
 
 			join teams_games on teams_games.game_id = games.id 
 			join team_club_players on team_club_players.team_id = teams_games.team_id
+			join club_players on club_players.id = team_club_players.id
+			join club_persons on club_persons.id = club_players.club_person_id
+			join persons on persons.id = club_persons.person_id
+
 		where 
 			games.id = 1  
 				
