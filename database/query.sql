37234e7d763236c6b1be94a803a4e247d2386d80
Jim Breslin <jbreslin33@gmail.com>
2020-06-16 21:47:09 -0400

    players on team

--- database/query.sql
+++ database/query.sql
@@ -1,3 +1,5 @@
+
+
                 select distinct club_persons.id as club_person_id, games.id, games.event_date, games.arrival_time, games.start_time, games.end_time, games.address, games.coordinates, pitches.name as pitch_name, games.field_name, clubs.name as club_name, teams.name as team_name, teams.id as team_id, persons.first_name, persons.last_name
 
                 from games
@@ -14,12 +16,12 @@
                 join persons on persons.id=club_persons.person_id
                 join emails_persons on emails_persons.person_id=persons.id
 
-		full outer join club_players on club_players.club_person_id = club_persons.id
-		full outer join club_parents on club_parents.club_person_id = club_persons.id
+               full outer join club_players on club_players.club_person_id = club_persons.id
+               full outer join club_parents on club_parents.club_person_id = club_persons.id
 
-		full outer join team_club_players on team_club_players.team_id=teams.id
+               full outer join team_club_players on team_club_players.team_id=teams.id
 
-                where emails_persons.email_id = 21	
+                where emails_persons.email_id = 21     
 		;
 
 		select 
@@ -40,3 +42,19 @@
 			;
 
 
+                select
+                        --team_club_players.id as team_club_player_id
+                        games.id as game_id, team_club_players.id as players, persons.first_name, persons.last_name
+                from
+                        games
+
+                        join teams_games on teams_games.game_id = games.id
+                        join team_club_players on team_club_players.team_id = teams_games.team_id
+                        join club_players on club_players.id = team_club_players.id
+                        join club_persons on club_persons.id = club_players.club_person_id
+                        join persons on persons.id = club_persons.person_id
+
+                where
+                        games.id = 1
+
+                        ;
