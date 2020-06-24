47bf9d1846ff61bb06e89679ff7a3226e7a90eda
Jim Breslin <jbreslin33@gmail.com>
2020-06-24 12:19:11 -0400

    avail for prac

--- database/build/delete_club.sql
+++ database/build/delete_club.sql
@@ -56,6 +56,7 @@ CREATE OR REPLACE PROCEDURE p_delete_club(int, INOUT x int)
 LANGUAGE plpgsql
 AS $$
 DECLARE
+        rec RECORD;
         recA RECORD;
         recB RECORD;
         recC RECORD;
@@ -66,139 +67,18 @@ BEGIN
 
 	--not deleteing below
 
-	FOR recA IN
-		select id from team_club_players 
-		
-		where club_id = $1
-		
+	FOR rec IN
+		select team_club_players.id from team_club_players 
+			join club_players on club_players.id = team_club_players.club_player_id
+			join club_persons on club_persons.id = club_players.club_person_id	
+			
+		where club_persons.club_id = $1
 	LOOP
-		FOR recB IN
-			select id from club_players where club_person_id = recA.id
-		LOOP
-			FOR recC IN
-				select id from team_club_players where club_player_id = recB.id
-			LOOP
-				delete from practices_players_availability where team_club_player_id = recC.id;		
-			END LOOP;
-		END LOOP;			
+		delete from practices_players_availability where team_club_player_id = rec.id;		
 	END LOOP;	
 
 
 
-
-        FOR recA IN
-		select id from teams where club_id = $1  
-        LOOP
-		FOR recB IN
-			select team_club_persons.id from team_club_persons where team_id = recA.id	
-		LOOP
-			delete from team_club_players where team_club_person_id = recB.id; 	
-		END LOOP;
-        END LOOP;
-
-        FOR recA IN
-		select id from club_persons where club_id = $1  
-        LOOP
-			delete from club_players where club_person_id = recA.id; 	
-        END LOOP;
-
-
-        FOR recA IN
-		select id from club_persons where club_id = $1  
-	LOOP
-		FOR recB IN
-			select team_club_persons.id from team_club_persons where club_person_id = recA.id	
-		LOOP
-			--delete from team_club_persons_club_administrators where team_club_person_id = recB.id; 	
-		END LOOP;
-	END LOOP;
-
-
-        FOR recA IN
-		select id from club_persons where club_id = $1  
-        LOOP
-			delete from club_administrators where club_person_id = recA.id; 	
-        END LOOP;
-
-
-        FOR recA IN
-		select id from club_persons where club_id = $1  
-	LOOP
-		FOR recB IN
-			select team_club_persons.id from team_club_persons where club_person_id = recA.id	
-		LOOP
-			delete from team_club_players  where team_club_person_id = recB.id; 	
-		END LOOP;
-	END LOOP;
-
-	--delete teams_practices
-        FOR recA IN
-                select practice_id from teams_practices where club_id = $1
-        LOOP
-        	FOR recA IN
-                	select id from practices where id = recA.practice_id 
-		LOOP
-			--delete from practice where practice_id
-
-		END LOOP;
-
-                delete from teams_practices where team_id = recA.id;
-	END LOOP;
-
-
-        FOR recA IN
-		select id from teams where club_id = $1
-	LOOP
-		FOR recB IN
-			select id from practice where team_id = recA.id
-		LOOP
-			delete from practices where practice_id = recB.id;	
-		END LOOP;
-	END LOOP;
-
-        FOR recA IN
-		select id from teams where club_id = $1
-	LOOP
-		delete from practice where team_id = recA.id;	
-	END LOOP;
-			
-        FOR recA IN
-		select id from club_persons where club_id = $1  
-	LOOP
-		FOR recB IN
-			select team_club_persons.id from team_club_persons where club_person_id = recA.id	
-		LOOP
-			--delete from team_club_persons_club_administrators where team_club_person_id = recB.id; 	
-		END LOOP;
-	END LOOP;
-
-
-        FOR recA IN
-		select id from club_persons where club_id = $1  
-	LOOP
-		FOR recB IN
-			select team_club_persons.id from team_club_persons where club_person_id = recA.id	
-		LOOP
-			delete from team_club_managers where team_club_person_id = recB.id; 	
-		END LOOP;
-	END LOOP;
-
-        FOR recA IN
-		select id from club_persons where club_id = $1  
-	LOOP
-		delete from team_club_persons where club_person_id = recA.id;
-	END LOOP;
-
-        FOR recA IN
-		select id from club_persons where club_id = $1  
-	LOOP
-		delete from club_managers where club_person_id = recA.id;
-	END LOOP;
-
-	delete from club_persons where club_id = $1;
-	delete from teams where club_id = $1;
-	delete from pitches where club_id = $1;
-	delete from clubs where id = $1 returning id into x;
 
 END;
 $$;
