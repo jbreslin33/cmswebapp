b2a91ceec1627541f59b7fb7ca80ea446922d162
Jim Breslin <jbreslin33@gmail.com>
2020-12-10 02:48:47 +0000

    fixed bug on rm club_parent on club_profile screen BUT need to do for player,coach manager etc.

--- php/classes/screens/delete_club_parent.php
+++ php/classes/screens/delete_club_parent.php
@@ -12,7 +12,9 @@ class DeleteClubParent extends Screen
         {
                 $sql = 'select f_delete_club_parent($1,$2,$3,$4)';
                 $prepare_result = pg_prepare($this->mDatabase->mConnection, "f_delete_club_parent", $sql);
-                $result = pg_execute($this->mDatabase->mConnection, "f_delete_club_parent", array( $this->getSenderEmailId(), $this->mPersonId, $this->mScreenPersonId, $this->mClubId));
+
+		$result = pg_execute($this->mDatabase->mConnection, "f_delete_club_parent", array( $this->mFamilyId, $this->mPersonId, $this->mScreenPersonId, $this->mClubId));
+
 
                 return pg_fetch_result($result, 0);
         }
